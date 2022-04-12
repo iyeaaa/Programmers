635becbe1cae3768ae520d22df7f14214a8eb11b
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-04-12 11:18:31 +0900

    Update 통계학.swift

--- 백준/Silver/2108. 통계학/통계학.swift
+++ 백준/Silver/2108. 통계학/통계학.swift
@@ -1,44 +1,29 @@
 import Foundation
 
 var array = [Int]()
+var dict = [Int:Int]()
 let n = Int(readLine()!)!
-var count = [Int](repeating: 0, count: 8002)
 var result = ""
-var bin = 0
-var binindex = 0
+var sum = 0
 
 for _ in 0..<n {
-    array.append(Int(readLine()!)!)
+    let num = Int(readLine()!)!
+    array.append(num)
+    sum += num
+    dict[num, default: 0] += 1
 }
 
-let sum = array.reduce(0){$0+$1}
-array.sort()
-var average = Int(round(Double(sum)/Double(n)))
+array = array.sorted()
+var maxValue = dict.max(by: {$0.value < $1.value})!.value
+var mode = dict.filter({$0.value == maxValue}).keys.sorted()
 
-for i in array {
-    count[i+4000] += 1
-}
-
-bin = count.max()!
-
-var j = 0
-for i in 0...8001 {
-    if bin == count[i] {
-        if j == 1 {
-            binindex = i - 4000
-        }
-        j += 1
-    }
-}
-
-if j == 1 {
-    binindex = count.index(of: bin)! - 4000
-}
-
-result += "\(average)\n"
-result += "\(array[(n-1)/2])\n"
-result += "\(binindex)\n"
-result += "\(array[n-1]-array[0])"
-
+// 산술평균
+result += "\(Int(round(Double(sum)/Double(n))))\n"
+// 중앙값
+result += "\(array[n/2])\n"
+// 최빈값
+result += mode.count == 1 ? "\(mode[0])\n" : "\(mode[1])\n"
+// 범위
+result += "\(array[n-1] - array[0])"
 
 print(result)
