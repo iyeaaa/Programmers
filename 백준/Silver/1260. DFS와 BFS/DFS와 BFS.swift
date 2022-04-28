d75da818c3f6cd72b03c2602a2df0f6820c2f921
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-04-28 14:05:33 +0900

    Update DFS와 BFS.swift

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
@@ -1,51 +1,52 @@
-let list = readLine()!.split{$0==" "}.map{Int(String($0))!}
-var node = [[Int]](repeating: [], count: list[0]+1)
-var dfs_visited = [Bool](repeating: false, count: list[0]+1)
-var bfs_visited = [Bool](repeating: false, count: list[0]+1)
+let nmv = readLine()!.split{$0==" "}.map{Int(String($0))!}
+var point = [[Int]](repeating: [], count: nmv[0]+1)
+var dfs_visit = [Bool](repeating: false, count: nmv[0]+1)
+var bfs_visit = [Bool](repeating: false, count: nmv[0]+1)
 var dfs_result = ""
 var bfs_result = ""
 
-for _ in 1...list[1] {
+for _ in 0..<nmv[1] {
     let temp = readLine()!.split{$0==" "}.map{Int(String($0))!}
-    node[temp[0]].append(temp[1])
-    node[temp[1]].append(temp[0])
+    point[temp[0]].append(temp[1])
+    point[temp[1]].append(temp[0])
 }
 
-for i in 1...list[0] {
-    node[i].sort(by: <)
+for i in 1...nmv[0] {
+    point[i].sort()
 }
 
+dfs(nmv[2])
+bfs(nmv[2])
 
-func dfs(_ start: Int) {
-    dfs_visited[start] = true
-    dfs_result += "\(start) "
-    
-    for i in node[start] {
-        if !dfs_visited[i] {
-            dfs(i)
-        }
-    }
-}
-
-
-func bfs(_ start: Int) {
-    var queue: [Int] = [start]
-    bfs_visited[start] = true
-    bfs_result += "\(start) "
-    
-    while !queue.isEmpty {
-        let temp = queue.removeFirst()
-        for i in node[temp] {
-            if !bfs_visited[i] {
-                queue.append(i)
-                bfs_result += "\(i) "
-                bfs_visited[i] = true
-            }
-        }
-    }
-}
-
-dfs(list[2])
-bfs(list[2])
 print(dfs_result)
 print(bfs_result)
+
+func dfs(_ start: Int) {
+    if dfs_visit[start] {
+        return
+    }
+    dfs_visit[start] = true
+    dfs_result += "\(start) "
+    for i in 0..<point[start].count {
+        dfs(point[start][i])
+    }
+}
+
+func bfs(_ start: Int) {
+    bfs_result += "\(start) "
+    bfs_visit[start] = true
+    var queue = [start]
+    var index = 0
+    var n = 1
+    while index < n {
+        for i in point[queue[index]] {
+            if !bfs_visit[i] {
+                queue.append(i)
+                bfs_visit[i] = true
+                n += 1
+                bfs_result += "\(i) "
+            }
+        }
+        index += 1
+    }
+}
