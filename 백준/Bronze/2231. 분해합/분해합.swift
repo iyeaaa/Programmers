720a59372789eed3e4ee1a9fbb03933b21a4e5a1
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-03 20:55:31 +0900

    [Bronze II] Title: 분해합, Time: 304 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.swift
+++ 백준/Bronze/2231. 분해합/분해합.swift
@@ -1,18 +1,17 @@
 import Foundation
 
 let n = Int(readLine()!)!
-var min = Int.max
+var min = 0
 
 for i in 0..<n {
     var cons = i
-    cons += Array(String(i)).map{Int(String($0))!}.reduce(0){$0+$1}
-    if cons == n && i < min {
+    for c in String(i) {
+        cons += Int(String(c))!
+    }
+    if cons == n {
         min = i
+        break
     }
 }
 
-if min == Int.max {
-    print(0)
-} else {
-    print(min)
-}
+print(min)
