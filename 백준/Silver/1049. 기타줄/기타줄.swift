e6268396efb1fac27a3060c48e783e62ed6f7c30
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-09 20:45:07 +0900

    [Silver IV] Title: 기타줄, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.swift
+++ 백준/Silver/1049. 기타줄/기타줄.swift
@@ -1,16 +1,15 @@
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (n, m) = (input[0], input[1])
-var price = [[Int]](repeating: [Int](repeating: 0, count: 2), count: m)
 
 var packageMin = Int.max
 var eachMin = Int.max
 for i in 0..<m {
-    price[i] = readLine()!.split{$0==" "}.map{Int(String($0))!}
-    if price[i][0] < packageMin {
-        packageMin = price[i][0]
+    let temp = readLine()!.split{$0==" "}.map{Int(String($0))!}
+    if temp[0] < packageMin {
+        packageMin = temp[0]
     }
-    if price[i][1] < eachMin {
-        eachMin = price[i][1]
+    if temp[1] < eachMin {
+        eachMin = temp[1]
     }
 }
 
