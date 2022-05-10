f3d1a517b03c98f5e0e6d0b8ecd8cfcc3e9e38ee
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-10 13:11:17 +0900

    [Gold V] Title: 검문, Time: 960 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Gold/2981. 검문/검문.swift
@@ -0,0 +1,84 @@
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        while now != 10
+                      && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+
+        return str
+    }
+}
+
+let file = FileIO()
+
+let n = file.readInt()
+var array = [Int]()
+var result = ""
+
+for _ in 0..<n {
+    array.append(file.readInt())
+}
+
+array.sort()
+
+var first = array[1]-array[0]
+for i in 2..<array.count {
+    first = gcd(first, array[i]-array[i-1])
+}
+
+for i in stride(from: 2, through: first/2, by: 1) {
+    if first % i == 0 {
+        result += "\(i) "
+    }
+}
+
+print(result + "\(first)")
+
+func gcd(_ ta: Int, _ tb: Int) -> Int {
+    var (a, b, r) = (ta, tb, ta%tb)
+    while r != 0 {
+        a = b
+        b = r
+        r = a % b
+    }
+    return b
+}
