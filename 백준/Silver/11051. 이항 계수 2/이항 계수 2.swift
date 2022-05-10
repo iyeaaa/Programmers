2e542826aa7e90dfd6c1008c0a38cfbf3ddd391d
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-10 14:51:29 +0900

    [Silver I] Title: 이항 계수 2, Time: 28 ms, Memory: 76988 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.swift
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.swift
@@ -2,7 +2,7 @@ let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (n, k) = (input[0], input[1])
 var dp = [[Int]](repeating: [Int](repeating: 0, count: k+1), count: n+1)
 
-for i in 0...n {
+for i in 1...n {
     for j in 0...min(i, k) {
         if j == 0 || j == i {
             dp[i][j] = 1
