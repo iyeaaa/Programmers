d4fd8509a242e5e8f4ee4e22b6b06aba018372df
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-13 22:06:50 +0900

    [Silver I] Title: A → B, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.swift
+++ 백준/Silver/16953. A → B/A → B.swift
@@ -1,26 +1,18 @@
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
-let (a, b) = (input[0], input[1])
-var counting = [Int:Int]()
+let a = input[0]
+var b = input[1]
+var count = 1
 
-print(bfs(start: a, target: b))
-
-func bfs(start: Int, target: Int) -> Int {
-    var index = 0
-    var queue = [start]
-    counting[start] = 1
-    while index < queue.count {
-        let cur = queue[index]
-        for i in [cur*2, Int(String(cur)+"1")!] {
-            if i <= 1_000_000_000 {
-                if i == target {
-                    return counting[cur]! + 1
-                } else {
-                    queue.append(i)
-                    counting[i] = counting[cur]! + 1
-                }
-            }
-        }
-        index += 1
+while a < b {
+    if b % 2 == 0 {
+        count += 1
+        b /= 2
+    } else if b % 10 == 1 {
+        count += 1
+        b /= 10
+    } else {
+        break
     }
-    return -1
 }
+
+print( a == b ? count : -1 )
