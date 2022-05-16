1bfec8693b33b689327efe6a7501b9a13ae8ade1
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-16 20:38:42 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.swift
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.swift
@@ -2,22 +2,21 @@ import Foundation
 
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (h, y) = (input[0], input[1])
-var dp = [Int](repeating: 0, count: 11)
-dp[0] = h
+var result = 0
+dfs(year: 0, money: h)
+print(result)
 
-for i in 1...2 {
-    dp[i] = Int(floor(Double(dp[i-1])*1.05))
+func dfs(year: Int, money: Int) {
+    if year > y {
+        return
+    } else if year == y {
+        result = max(result, money)
+        return
+    } else {
+        dfs(year: year+1, money: money*105/100)
+        dfs(year: year+3, money: money*120/100)
+        dfs(year: year+5, money: money*135/100)
+    }
 }
 
-for i in 3...4 {
-    dp[i] = max(Int(floor(Double(dp[i-1])*1.05))
-            , Int(floor(Double(dp[i-3])*1.2)))
-}
-
-for i in stride(from: 5, through: y, by: 1) {
-    dp[i] = max(Int(floor(Double(dp[i-1])*1.05))
-            , Int(floor(Double(dp[i-3])*1.2))
-            , Int(floor(Double(dp[i-5])*1.35)))
-}
-
-print(dp[y])
+// wapas님 많이 배우고갑니다.
