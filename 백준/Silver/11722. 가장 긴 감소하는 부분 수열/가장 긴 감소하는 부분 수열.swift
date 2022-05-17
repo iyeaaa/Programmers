f82506a7c8dc527fc9b89adf8dbf486f92ce3b61
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-17 11:37:46 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.swift
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.swift
@@ -2,10 +2,10 @@ let n = Int(readLine()!)!
 let list = readLine()!.split{$0==" "}.map{Int(String($0))!}
 var dp = [Int](repeating: 1, count: n)
 
-for i in 1..<list.count {
+for i in 1..<n {
     for j in 1...i {
-        if list[i] < list[i-j] {
-            dp[i] = max(dp[i], dp[i-j]+1)
+        if list[i] < list[i-j] && dp[i-j] >= dp[i] {
+            dp[i] = dp[i-j] + 1
         }
     }
 }
