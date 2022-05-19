d5f137126ccc9cf14b7ea6eec7955cb8acb39c7b
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-19 12:41:56 +0900

    [Gold IV] Title: 내리막 길, Time: 104 ms, Memory: 73324 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.swift
+++ 백준/Gold/1520. 내리막 길/내리막 길.swift
@@ -2,7 +2,6 @@ let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (n, m) = (input[0], input[1])
 var graph = [[Int]](repeating: [], count: n+1)
 var dp = [[Int]](repeating: [Int](repeating: -1, count: m), count: n)
-var safe = [[Int]]()
 let direction = [[-1, 0], [0, -1], [1, 0], [0, 1]]
 
 for i in 0..<n {
