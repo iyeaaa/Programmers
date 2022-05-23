226aff56ba337f06645d1180d93faa799bb960aa
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-23 11:50:52 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 12 ms, Memory: 71072 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.swift
@@ -0,0 +1,41 @@
+let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+let (n, k) = (input[0], input[1])
+
+print(n == k ? 0 : bfs(start: n))
+
+func bfs(start: Int) -> Int {
+    var visit = [Int](repeating: -1, count: 100001)
+    visit[start] = 0
+    var queue = [start]
+    var index = 0
+    while index < queue.count {
+        let cur = queue[index]
+
+        if (0...100000).contains(cur*2) && visit[cur*2] == -1 {
+            queue.append(cur*2)
+            visit[cur*2] = visit[cur]
+            if cur*2 == k {
+                return visit[cur*2]
+            }
+        }
+
+        if (0...100000).contains(cur-1) && visit[cur-1] == -1 {
+            queue.append(cur-1)
+            visit[cur-1] = visit[cur] + 1
+            if cur-1 == k {
+                return visit[cur-1]
+            }
+        }
+
+        if (0...100000).contains(cur+1) && visit[cur+1] == -1 {
+            queue.append(cur+1)
+            visit[cur+1] = visit[cur] + 1
+            if cur+1 == k {
+                return visit[cur+1]
+            }
+        }
+
+        index += 1
+    }
+    return -1
+}
