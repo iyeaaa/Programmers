30e513cf62d22e537b074c0901d96591f85a2614
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-24 11:08:30 +0900

    [Silver III] Title: 랜선 자르기, Time: 16 ms, Memory: 69240 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.swift
@@ -0,0 +1,25 @@
+let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+let (k, n) = (input[0], input[1])
+var ran = [Int]()
+
+for _ in 0..<k {
+    ran.append(Int(readLine()!)!)
+}
+
+print(binary_Search(list: ran, start: 1, end: ran.max()!, target: n))
+
+func binary_Search(list: [Int], start: Int, end: Int, target: Int) -> Int {
+    var start = start
+    var end = end
+    var result = 0
+    while start <= end {
+        let mid = (start+end) / 2
+        var mid_Sum = list.reduce(0) { $0 + ($1 / mid) }
+        if target > mid_Sum {
+            end = mid - 1
+        } else {
+            start = mid + 1
+        }
+    }
+    return end
+}
