63dbff25b2f5762f248580693512b1e362691a08
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-24 13:14:06 +0900

    [Silver III] Title: 예산, Time: 12 ms, Memory: 70020 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.swift
+++ 백준/Silver/2512. 예산/예산.swift
@@ -7,16 +7,16 @@ print(binary_Search(list: money, start: 0, end: money.max()!, target: my_money))
 func binary_Search(list: [Int], start: Int, end: Int, target: Int) -> Int {
     var start = start
     var end = end
+    var result = 0
     while start <= end {
         let mid = (start+end) / 2
         let mid_sum = list.reduce(0) { $0 + (($1 > mid) ? mid : $1) }
-        if mid_sum == target {
-            return mid
-        } else if target < mid_sum {
+        if target < mid_sum {
             end = mid - 1
         } else {
             start = mid + 1
+            result = mid
         }
     }
-    return end
+    return result
 }
