a68701efdf82de4fd125ca616331e63ef527abc4
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-29 21:38:56 +0900

    Update WapasIO.swift

--- WapasIO.swift
+++ WapasIO.swift
@@ -1,15 +1,15 @@
-import Foundation
-
-var bytes: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), byteIdx = 0
+var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), byteIdx = 0
+buffer.append(0)
 
 @inline(__always) func readByte() -> UInt8 {
     defer { byteIdx += 1 }
-    return bytes.withUnsafeBufferPointer { $0[byteIdx] }
+    return buffer[byteIdx]
 }
 
 @inline(__always) func readInt() -> Int {
-    var number = 0, byte = readByte()
+    var number = 0, byte = readByte(), isNegative = false
     while byte == 10 || byte == 32 { byte = readByte() }
+    if byte == 45 { byte = readByte(); isNegative = true }
     while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
-    return number
+    return number * (isNegative ? -1 : 1)
 }
