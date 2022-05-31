953954363da231f37483527ac95cf2b495e4d59a
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-05-31 14:29:54 +0900

    Update WapasIO.swift

--- WapasIO.swift
+++ WapasIO.swift
@@ -8,8 +8,8 @@ buffer.append(0)
 
 @inline(__always) func readInt() -> Int {
     var number = 0, byte = readByte(), isNegative = false
-    while byte == 10 || byte == 32 { byte = readByte() }
-    if byte == 45 { byte = readByte(); isNegative = true }
-    while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
+    while byte == 10 || byte == 32 { byte = readByte() }    // 공백문자 10->줄바꿈문자, 32->공백문자
+    if byte == 45 { byte = readByte(); isNegative = true }  // 45 -> '-'
+    while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() } // 48~57 -> 0~9 숫자들
     return number * (isNegative ? -1 : 1)
 }
