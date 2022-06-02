e0e60061c76a4162438ec04c16effdea00b090fd
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-03 00:43:04 +0900

    [Gold V] Title: ABCDE, Time: 256 ms, Memory: 79640 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.swift
+++ 백준/Gold/13023. ABCDE/ABCDE.swift
@@ -9,9 +9,8 @@ buffer.append(0)
 }
 
 @inline(__always) func readInt() -> Int {
-    var number = 0, byte = readByte(), isNegative = false
+    var number = 0, byte = readByte()
     while byte == 10 || byte == 32 { byte = readByte() }
-    if byte == 45 { byte = readByte(); isNegative = true }
     while 48...57 ~= byte { number = 10 * number + Int(byte-48); byte = readByte() }
     return number
 }
@@ -19,33 +18,28 @@ buffer.append(0)
 let (n, m) = (readInt(), readInt())
 var graph = [[Int]](repeating: [], count: n)
 var visit = [Bool](repeating: false, count: n)
-for i in 0..<m {
+for _ in 0..<m {
     let (a, b) = (readInt(), readInt())
     graph[a].append(b)
     graph[b].append(a)
 }
 
 for i in 0..<n {
-    visit[i] = true
     dfs(i, 0)
-    visit[i] = false
 }
 
 print(0)
 
 func dfs(_ x: Int, _ digit: Int) {
+    visit[x] = true
     if digit == 4 {
         print(1)
         exit(0)
     }
-    if graph[x] == [] {
-        return
-    }
     for i in graph[x] {
         if !visit[i] {
-            visit[i] = true
             dfs(i, digit + 1)
-            visit[i] = false
         }
     }
+    visit[x] = false
 }
