3249560932af6626d2501cd1c5f2be904d424d3c
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-08 17:03:48 +0900

    Update WapasIO.swift

--- WapasIO.swift
+++ WapasIO.swift
@@ -1,15 +1,32 @@
-var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), byteIdx = 0
-buffer.append(0)
+import Foundation
+
+var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), byteIdx = 0; buffer.append(0)
 
 @inline(__always) func readByte() -> UInt8 {
     defer { byteIdx += 1 }
-    return buffer[byteIdx]
+    return buffer.withUnsafeBufferPointer { $0[byteIdx] }
 }
 
 @inline(__always) func readInt() -> Int {
-    var number = 0, byte = readByte(), isNegative = false
-    while byte == 10 || byte == 32 { byte = readByte() }    // 공백문자 10->줄바꿈문자, 32->공백문자
-    if byte == 45 { byte = readByte(); isNegative = true }  // 45 -> '-'
-    while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() } // 48~57 -> 0~9 숫자들
-    return number * (isNegative ? -1 : 1)
+    var number = 0, byte = readByte()
+    while !(48...57 ~= byte) { byte = readByte() }
+    while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
+    return number
+}
+
+@inline(__always) func readCharacter() -> UInt8 {
+    var byte = readByte()
+    while byte == 10 || byte == 32 { byte = readByte() }
+    return byte
+}
+
+@inline(__always) func readCharacters() -> [UInt8] {
+    var byte = readByte(), characters = [UInt8]()
+    while !(68...82 ~= byte) { byte = readByte() }
+    while 68...82 ~= byte { characters.append(byte); byte = readByte() }
+    return characters
+}
+
+@inline(__always) func writeByString(_ output: String) {
+    FileHandle.standardOutput.write(output.data(using: .utf8)!)
 }
