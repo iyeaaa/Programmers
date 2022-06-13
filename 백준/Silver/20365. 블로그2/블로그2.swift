fced39235db114d5d62c058bc8804a2bf9bdf6df
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-13 10:37:01 +0900

    [Silver II] Title: 블로그2, Time: 32 ms, Memory: 80520 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.swift
+++ 백준/Silver/20365. 블로그2/블로그2.swift
@@ -1,22 +1,19 @@
+import Foundation
+
 let n = Int(readLine()!)!
-let color = Array(readLine()!)
-var blue = 0, red = 0
-var preblue = false
-
-if color[0] == "B" {
-    blue += 1; preblue = true
-} else {
-    red += 1
-}
-
-for c in 1..<color.count {
-    if color[c] == "B" && !preblue {
-        blue += 1
-        preblue = true
-    } else if color[c] == "R" && preblue {
-        red += 1
-        preblue = false
+let input = readLine()!
+var bCnt = 0
+var rCnt = 0
+var chr = ""
+input.forEach {
+    if String($0) != chr {
+        if $0 == "B" {
+            bCnt += 1
+            chr = "B"
+        } else {
+            rCnt += 1
+            chr = "R"
+        }
     }
 }
-
-print(blue < red ? blue + 1 : red + 1)
+print(1+min(bCnt, rCnt))
