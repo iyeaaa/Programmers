203dc75b73ed410cb6beb62efd9175a6f3cf40d0
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-13 22:57:52 +0900

    [Silver II] Title: 민겸 수, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Silver/21314. 민겸 수/민겸 수.swift
+++ 백준/Silver/21314. 민겸 수/민겸 수.swift
@@ -1,4 +1,5 @@
 // 둘다 K가 나올때까지 M을 카운트, K가 나오면 K까지 or K 전까지, 만약 나오지 않는다면 최대한 잘게 or 모두
+// 나 진짜 못하네; ㅋㅋ
 
 let str = readLine()!
 
@@ -8,18 +9,15 @@ print(findMin())
 func findMax() -> String {
     var countM = 0
     var max = ""
-    var lastIsK = false
     for s in str {
         if s == "M" {
             countM += 1
-            lastIsK = false
         } else {
             max += "5" + String(repeating: "0", count: countM)
             countM = 0
-            lastIsK = true
         }
     }
-    if !lastIsK {
+    if countM > 0 {
         max += String(repeating: "1", count: countM)
     }
     return max
@@ -27,21 +25,18 @@ func findMax() -> String {
 
 func findMin() -> String {
     var min = ""
-    var lastIsK = false
     var countM = 0
     for s in str {
         if s == "M" {
             countM += 1
-            lastIsK = false
         } else {
             if countM > 0 {
                 min += "1" + String(repeating: "0", count: countM-1)
             }
             min += "5"; countM = 0
-            lastIsK = true
         }
     }
-    if !lastIsK && countM > 0 {
+    if countM > 0 {
         min += "1" + String(repeating: "0", count: countM-1)
     }
     return min
