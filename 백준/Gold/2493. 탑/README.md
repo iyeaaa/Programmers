8eec8c46008a999ca735bcced04ed2e9c5e92c34
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-27 00:36:34 +0900

    [Gold V] Title: 탑, Time: 464 ms, Memory: 106148 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/README.md
@@ -0,0 +1,12 @@
+# [Gold V] 탑 - 2493 
+
+[문제 링크](https://www.acmicpc.net/problem/2493) 
+
+### 성능 요약
+
+메모리: 106148 KB, 시간: 464 ms
+
+### 분류
+
+자료 구조(data_structures), 스택(stack)
+
---
+++ 백준/Gold/2493. 탑/탑.swift
@@ -0,0 +1,26 @@
+let n = Int(readLine()!)!
+let list = readLine()!.split{$0==" "}.map{Int(String($0))!}
+var stack = [0]
+var dict = [0: 0];
+
+for i in 1..<n {
+    while !stack.isEmpty && list[stack.last!] < list[i] {
+        let _ = stack.popLast()
+    }
+    if !stack.isEmpty {
+        dict[i] = stack.last!+1
+    } else {
+        dict[i] = 0
+    }
+    stack.append(i)
+}
+
+var result = ""
+for i in 0..<n {
+    if let d = dict[i] {
+        result += "\(d) "
+    } else {
+        result += "0 "
+    }
+}
+print(result)
