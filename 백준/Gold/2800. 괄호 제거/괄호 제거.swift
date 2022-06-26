f3d5edff76d408dc751c2b51d71fe3a6c4cf8805
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-26 15:31:14 +0900

    [Gold V] Title: 괄호 제거, Time: 20 ms, Memory: 69368 KB -BaekjoonHub

---
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.swift
@@ -0,0 +1,34 @@
+var strn = Array(readLine()!).map{String($0)}
+var pairs = crtPairArray(strn); let nmbrPairs = pairs.count
+var dict = [String:Int]()
+
+dfs(strn, 0)
+dict.map{$0.key}.sorted().forEach{ print($0) }
+
+func dfs(_ strn: [String], _ digit: Int) {
+    if digit == nmbrPairs { return }
+    let strn1 = convertWhiteSpace(strn, pairs[digit])
+    let strn2 = removalWhiteSpaceToString(strn1)
+    dict[strn2] = 1
+    dfs(strn1, digit+1)
+    dfs(strn, digit+1)
+}
+
+func crtPairArray(_ strn: [String]) -> [(Int, Int)] {
+    var result = [(Int, Int)]()
+    var stack = [Int]()
+    for i in 0..<strn.count {
+        if strn[i] == "(" { stack.append(i) }
+        else if strn[i] == ")" { result.append((stack.popLast()!, i)) }
+    }
+    return result
+}
+func removalWhiteSpaceToString(_ strn: [String]) -> String {
+    strn.filter{ $0 != " " }.joined()
+}
+func convertWhiteSpace(_ strn: [String], _ pairIndex: (Int, Int)) -> [String] {
+    var strn = strn
+    let (left, right) = pairIndex
+    strn[left] = " "; strn[right] = " "
+    return strn
+}
