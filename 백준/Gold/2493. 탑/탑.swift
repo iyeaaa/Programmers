273ff9bdbb3d918c6ff692e41c7dbbcf69095897
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-27 00:39:54 +0900

    [Gold V] Title: 탑, Time: 356 ms, Memory: 106144 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.swift
+++ 백준/Gold/2493. 탑/탑.swift
@@ -1,26 +1,18 @@
 let n = Int(readLine()!)!
 let list = readLine()!.split{$0==" "}.map{Int(String($0))!}
 var stack = [0]
-var dict = [0: 0];
+var result = "0 "
 
 for i in 1..<n {
     while !stack.isEmpty && list[stack.last!] < list[i] {
         let _ = stack.removeLast()
     }
     if !stack.isEmpty {
-        dict[i] = stack.last!+1
+        result += "\(stack.last! + 1) "
     } else {
-        dict[i] = 0
+        result += "0 "
     }
     stack.append(i)
 }
 
-var result = ""
-for i in 0..<n {
-    if let d = dict[i] {
-        result += "\(d) "
-    } else {
-        result += "0 "
-    }
-}
 print(result)
