c354f185ef1249411673da54c74d97ebaec3a2d1
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-27 14:46:17 +0900

    Update 나무 자르기.swift
    조금 더 간결하게

--- 백준/Silver/2805. 나무 자르기/나무 자르기.swift
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.swift
@@ -1,22 +1,16 @@
-import Foundation
+func isPsb(_ h: Int) -> Bool { tree.map{$0 >= h ? $0-h : 0}.reduce(0, +) >= m }
 
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (n, m) = (input[0], input[1])
 let tree = readLine()!.split{$0==" "}.map{Int(String($0))!}
 
-print(binary_Search(list: tree, start: 0, end: tree.max()!, target: m))
-
-func binary_Search(list: [Int], start: Int, end: Int, target: Int) -> Int {
-    var start = start
-    var end = end
-    while start <= end {
-        let mid = (start + end) / 2
-        let sum = tree.map{ $0 > mid ? $0 - mid : 0 }.reduce(0, +)
-        if target > sum {
-            end = mid - 1
-        } else {
-            start = mid + 1
-        }
+var (lf, ryt) = (0, tree.max()!)
+while lf <= ryt {
+    let mid = (lf + ryt) / 2
+    if isPsb(mid) {
+        lf = mid + 1
+    } else {
+        ryt = mid - 1
     }
-    return end
 }
+print(ryt)
