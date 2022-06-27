4ec9d2f62b42810671d8de88830b0decc8985f97
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-27 20:13:43 +0900

    [Silver III] Title: 선분 위의 점, Time: 296 ms, Memory: 77276 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.swift
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.swift
@@ -1,39 +1,37 @@
 var input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (n, m) = (input[0], input[1])
 let x = readLine()!.split{$0==" "}.map{Int(String($0))!}.sorted()
+var line = [(Int, Int)]()
+
 var result = ""
-
-for i in 0..<m {
+for _ in 0..<m {
     input = readLine()!.split{$0==" "}.map{Int(String($0))!}
-    result += "\(bs2(input[1]) - bs1(input[0]) + 1)\n"
+    result += "\(bnryBelowSrch(input[1])-bnryOverSrch(input[0])+1)\n"
 }
-
 print(result)
 
-func bs1(_ n: Int) -> Int {
-    var start = 0
-    var end = x.count - 1
-    while start <= end {
-        let mid = (start + end) / 2
-        if x[mid] < n {
-            start = mid + 1
+func bnryOverSrch(_ strt: Int) -> Int {
+    var (lf, ryt) = (0, x.count-1)
+    while lf <= ryt {
+        let mid = (lf + ryt) / 2
+        if strt <= x[mid] {
+            ryt = mid - 1
         } else {
-            end = mid - 1
+            lf = mid + 1
         }
     }
-    return start
+    return lf
+}
+func bnryBelowSrch(_ end: Int) -> Int {
+    var (lf, ryt) = (0, x.count-1)
+    while lf <= ryt {
+        let mid = (lf + ryt) / 2
+        if x[mid] <= end {
+            lf = mid + 1
+        } else {
+            ryt = mid - 1
+        }
+    }
+    return ryt
 }
 
-func bs2(_ n: Int) -> Int {
-    var start = 0
-    var end = x.count - 1
-    while start <= end {
-        let mid = (start + end) / 2
-        if x[mid] <= n {
-            start = mid + 1
-        } else {
-            end = mid - 1
-        }
-    }
-    return end
-}
