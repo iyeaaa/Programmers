08e3e9d50ce15b85592ee45749af9cc518780c2b
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-06-28 15:58:53 +0900

    [Silver III] Title: N과 M (4), Time: 24 ms, Memory: 69436 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.swift
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.swift
@@ -1,20 +1,19 @@
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (n, m) = (input[0], input[1])
-var result = ""
 var stack = [Int]()
+var result = ""
 
-recusion(0, 1)
+bkTrk(0, 1)
 print(result)
 
-func recusion(_ num: Int, _ crt: Int) {
-    if num == m {
-        result += stack.map{String($0)}.joined(separator: " ")
-        result += "\n"
-    } else {
-        for i in crt...n {
-            stack.append(i)
-            recusion(num+1, i)
-            stack.popLast()
-        }
+func bkTrk(_ digit: Int, _ next: Int) {
+    if digit == m {
+        result += stack.map{String($0)}.joined(separator: " ") + "\n"
+        return
+    }
+    for i in stride(from: next, through: n, by: 1) {
+        stack.append(i)
+        bkTrk(digit+1, i)
+        stack.removeLast()
     }
 }
