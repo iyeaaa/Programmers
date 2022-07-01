74ca7c0314391fcd042b77df6d918d202f918e7c
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-01 16:01:04 +0900

    [Silver III] Title: 문자열 집합, Time: 132 ms, Memory: 74956 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.swift
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.swift
@@ -1,18 +1,15 @@
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
-let (n, m) = (input[0], input[1])
-var set1 = Set<String>()
-var set2 = Set<String>()
-var count = 0
 
-for _ in 0..<n {
-    set1.insert(readLine()!)
+var set = Set<String>()
+
+for _ in 0..<input[0] {
+    set.insert(readLine()!)
 }
 
-for _ in 0..<m {
-    let data = readLine()!
-    if set1.contains(data) {
-        count += 1
-    }
+var count = 0
+
+for _ in 0..<input[1] {
+    if set.contains(readLine()!) { count += 1 }
 }
 
 print(count)
