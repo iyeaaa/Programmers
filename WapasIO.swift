27a1678754c5eabad72f6b898b3f47015a9d9347
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-03 14:22:30 +0900

    Update WapasIO.swift

--- WapasIO.swift
+++ WapasIO.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), byteIdx = 0; buffer.append(0)
 
 @inline(__always) func readByte() -> UInt8 {
@@ -8,23 +6,17 @@ var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), byt
 }
 
 @inline(__always) func readInt() -> Int {
-    var number = 0, byte = readByte()
-    while !(48...57 ~= byte) { byte = readByte() }
+    var number = 0, byte = readByte(), isNegative = false
+    while byte == 10 || byte == 32 { byte = readByte() }
+    if byte == 45 { byte = readByte(); isNegative = true }
     while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
-    return number
+    return number * (isNegative ? -1 : 1)
 }
 
 @inline(__always) func readCharacter() -> UInt8 {
     var byte = readByte()
     while byte == 10 || byte == 32 { byte = readByte() }
     return byte
-}
-
-@inline(__always) func readCharacters() -> [UInt8] {
-    var byte = readByte(), characters = [UInt8]()
-    while !(68...82 ~= byte) { byte = readByte() }
-    while 68...82 ~= byte { characters.append(byte); byte = readByte() }
-    return characters
 }
 
 @inline(__always) func writeByString(_ output: String) {
