680a114b1d8709234c4497aa5399eae00c8736ee
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-04 13:50:54 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.swift
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.swift
@@ -1,24 +1,18 @@
 import Foundation
 
 func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
-    let report = Set(report)
     var report_ID = [String: [String]]()
     var rptedCount = [String: Int]()
-    
-    for r in report {
+
+    for r in Set(report) {
         let rpt = r.split{$0==" "}.map{String($0)}
         report_ID[rpt[0], default: []].append(rpt[1])
         rptedCount[rpt[1], default: 0] += 1
     }
-    
-    var result = [Int]()
-    for i in id_list {
-        var count = 0
-        for r in (report_ID[i] ?? []) {
-            if rptedCount[r]! >= k { count += 1 }
+
+    return id_list.map { id in
+        report_ID[id, default: []].reduce(0) {
+            $0 + (rptedCount[$1, default: 0] >= k ? 1 : 0)
         }
-        result.append(count)
     }
-    
-    return result
 }
