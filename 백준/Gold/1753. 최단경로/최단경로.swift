37686b4cf3a6e93b9f799e8d1604d4345a230704
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-06 09:45:28 +0900

    [Gold IV] Title: 최단경로, Time: 108 ms, Memory: 102524 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.swift
+++ 백준/Gold/1753. 최단경로/최단경로.swift
@@ -1,140 +1,166 @@
 import Foundation
 
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
 
-public struct Heap<T> {
-  var nodes: [T] = []
-  let comparer: (T,T) -> Bool
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
 
-  var isEmpty: Bool {
-      return nodes.isEmpty
-  }
-
-  init(comparer: @escaping (T,T) -> Bool) {
-      self.comparer = comparer
-  }
-
-  func peek() -> T? {
-      return nodes.first
-  }
-
-  mutating func insert(_ element: T) {
-      var index = nodes.count
-
-      nodes.append(element)
-
-      while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) {
-          nodes.swapAt(index, (index-1)/2)
-          index = (index-1)/2
-      }
-  }
-
-  mutating func delete() -> T? {
-      guard !nodes.isEmpty else {
-          return nil
-      }
-
-      if nodes.count == 1 {
-          return nodes.removeFirst()
-      }
-
-      let result = nodes.first
-      nodes.swapAt(0, nodes.count-1)
-      _ = nodes.popLast()
-
-      var index = 0
-
-      while index < nodes.count {
-          let left = index * 2 + 1
-          let right = left + 1
-
-          if right < nodes.count {
-              if comparer(nodes[left], nodes[right]),
-                  !comparer(nodes[right], nodes[index]) {
-                  nodes.swapAt(right, index)
-                  index = right
-              } else if !comparer(nodes[left], nodes[index]){
-                  nodes.swapAt(left, index)
-                  index = left
-              } else {
-                  break
-              }
-          } else if left < nodes.count {
-              if !comparer(nodes[left], nodes[index]) {
-                  nodes.swapAt(left, index)
-                  index = left
-              } else {
-                  break
-              }
-          } else {
-              break
-          }
-      }
-
-      return result
-  }
-}
-
-extension Heap where T: Comparable {
-    init() {
-        self.init(comparer: >)
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
     }
-}
 
-// Heap By 라이노님 https://gist.github.com/JCSooHwanCho/a3f070c2160bb8c0047a5ddbb831f78e
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
 
-struct EdgeData : Comparable{
-    static func < (lhs: EdgeData, rhs: EdgeData) -> Bool {
-        lhs.cost < rhs.cost
+        return buffer[index]
     }
-    
-    var cost : Int
-    var node : Int
-}
 
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
 
-func solution(){
-    let inf = 7777777
-    let firstLine = readLine()!.split(separator: " ").map({Int(String($0))!})
-    let v = firstLine[0]
-    let e = firstLine[1]
-    
-    let start = Int(readLine()!)! - 1
-    
-    var graph = Array(repeating: [(Int,Int)]() , count: v)
-    for _ in 0..<e{
-        let line = readLine()!.split(separator: " ").map({Int(String($0))!})
-        graph[line[0]-1].append((line[1]-1,line[2]))
-    }
-    
-
-    var d = Array(repeating: inf, count: v)
-    d[start] = 0
-
-        
-    var pq: Heap = Heap<EdgeData>()
-    pq.insert(EdgeData(cost: 0,node: start))
-    while(!pq.isEmpty){
-        let now = pq.delete()!
-        if d[now.node] < now.cost{
-            continue
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
         }
-        
-        for next in graph[now.node]{
 
-            if now.cost + next.1 < d[next.0]{
-                d[next.0] = now.cost + next.1
-                pq.insert(EdgeData(cost: now.cost + next.1,node: next.0))
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+struct Heap<T: Comparable> {
+    var heap = [T]()
+
+    init() {}
+    init(_ h: T) { heap.append(h); heap.append(h) }
+
+    var isEmpty: Bool { heap.count <= 1 }
+
+    var first: T? { isEmpty ? nil : heap[1] }
+
+    mutating func insert(_ h: T) {
+        func haveToUp(_ isrtIdx: Int) -> Bool {
+            isrtIdx <= 1 ? false : heap[isrtIdx] < heap[isrtIdx/2]
+        }
+        heap.append(h)
+        if isEmpty { heap.append(h); return }
+
+        var isrtIdx = heap.count - 1
+        while haveToUp(isrtIdx) {
+            let prntIdx = isrtIdx / 2
+            heap.swapAt(isrtIdx, prntIdx)
+            isrtIdx = prntIdx
+        }
+    }
+
+    enum Direction { case none, lf, ryt }
+    mutating func pop() -> T? {
+        func haveToDown(_ popIdx: Int) -> Direction {
+            let lfIdx = popIdx * 2
+            let rytIdx = popIdx * 2 + 1
+            if lfIdx >= heap.count { return .none }
+            if rytIdx >= heap.count {
+                return heap[lfIdx] < heap[popIdx] ? .lf : .none
+            }
+            if heap[lfIdx] >= heap[popIdx] && heap[rytIdx] >= heap[popIdx] {
+                return .none
+            }
+            if heap[lfIdx] < heap[popIdx] && heap[rytIdx] < heap[popIdx] {
+                return heap[lfIdx] < heap[rytIdx] ? .lf : .ryt
+            }
+            return heap[lfIdx] < heap[popIdx] ? .lf : .ryt
+        }
+
+        if isEmpty { return nil }
+        heap.swapAt(1, heap.count-1)
+        let returnData = heap.popLast()!
+
+        var popIdx = 1
+        while true {
+            switch haveToDown(popIdx) {
+            case .none:
+                return returnData
+            case .lf:
+                let chlIdx = popIdx*2
+                heap.swapAt(popIdx, chlIdx)
+                popIdx = chlIdx
+            case .ryt:
+                let chlIdx = popIdx*2 + 1
+                heap.swapAt(popIdx, chlIdx)
+                popIdx = chlIdx
             }
         }
     }
-    for i in d{
-        if i == inf{
-            print("INF")
-        } else {
-            print(i)
-        }
-    }
-    
 }
 
-solution()
+struct Node: Comparable {
+    var no: Int
+    var dist: Int
+
+    static func < (lhs: Node, rhs: Node) -> Bool {
+        lhs.dist < rhs.dist
+    }
+}
+
+var io = IO()
+let INF = Int.max
+let (v, e) = (io.readInt(), io.readInt())
+let k = io.readInt()-1
+
+var graph: Array<[(Int, Int)]> = Array(repeating: [(Int, Int)](), count: v)
+for _ in 0..<e {
+    graph[io.readInt()-1].append((io.readInt()-1, io.readInt()))
+}
+
+var distance = Array(repeating: INF, count: v); distance[k] = 0
+
+var pq = Heap<Node>()
+pq.insert(Node(no: k, dist: 0))
+while !pq.isEmpty {
+    let now = pq.pop()!
+    if distance[now.no] < now.dist {
+        continue
+    }
+    for next in graph[now.no] {
+        if now.dist + next.1 < distance[next.0] {
+            distance[next.0] = now.dist + next.1
+            pq.insert(Node(no: next.0, dist: distance[next.0]))
+        }
+    }
+}
+
+var result = ""
+for i in distance {
+    result += (i == INF ? "INF\n" : "\(i)\n")
+}
+print(result)
