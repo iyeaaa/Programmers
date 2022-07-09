24504381ba7dc3f0abec835f738e002ff2ceb47d
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-09 19:14:09 +0900

    [Gold IV] Title: 이분 그래프, Time: 184 ms, Memory: 100508 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.swift
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.swift
@@ -1,55 +1,115 @@
-let Case = Int(readLine()!)!
-let (RED, BLUE, NON) = (-1, 1, 0)
+import Foundation
 
-first: for _ in 0..<Case {
-    let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
-    let (v, e) = (input[0], input[1])
-    var graph = [[Int]](repeating: [], count: v+1)
-    var visit = [Int](repeating: 0, count: 20001)
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
 
-    for _ in 0..<e {
-        let temp = readLine()!.split{$0==" "}.map{Int(String($0))!}
-        graph[temp[0]].append(temp[1])
-        graph[temp[1]].append(temp[0])
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
     }
 
-    for i in 1...v {
-        if visit[i] == 0 {
-            if bfs(start: i, graph: &graph, visit: &visit) == "NO" {
-                print("NO")
-                continue first
-            }
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
         }
+
+        return sum * (isPositive ? 1:-1)
     }
 
-    print("YES")
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
 }
 
-func bfs(start: Int, graph: inout [[Int]], visit: inout [Int]) -> String {
-    var queue = [start]
-    var index = 0
-    visit[start] = RED
-    while index < queue.count {
-        let cur = queue[index]
-        for i in graph[cur] {
-            if visit[i] == NON {
-                if visit[cur] == RED {
-                    visit[i] = BLUE
-                } else {
-                    visit[i] = RED
-                }
-                queue.append(i)
-            } else if visit[i] == BLUE {
-                if visit[cur] == BLUE {
-                    return "NO"
-                }
-            } else {    // visit[i] == RED
-                if visit[cur] == RED {
-                    return "NO"
-                }
-            }
-        }
-        index += 1
+struct UnionFind {
+    var same: [Int]
+    var dfrnt: [Int]
+
+    init(_ n: Int) { same = Array(0...n); dfrnt = Array(repeating: 0, count: n+1) }
+
+    mutating func root(of x: Int) -> Int {
+        if x == same[x] { return x }
+        same[x] = root(of: same[x])
+        return same[x]
     }
-    return "YES"
+
+    mutating func merge(_ x: Int, with y: Int) {
+        let (x, y) = (root(of: x), root(of: y))
+        if x == y { return }
+        same[y] = x
+    }
+
+    mutating func isUnion(_ x: Int, with y: Int) -> Bool {
+        root(of: x) == root(of: y)
+    }
 }
+
+
+let io = IO()
+var result = ""
+
+for _ in 0..<io.readInt() {
+    let (V, E) = (io.readInt(), io.readInt())
+    var ds = UnionFind(V)
+    var isYes = true
+    for _ in 0..<E {
+        let (u, v) = (ds.root(of: io.readInt()), ds.root(of: io.readInt()))
+        if u == v { isYes = false }
+
+        if ds.dfrnt[u] == 0 {
+            ds.dfrnt[u] = v
+        } else {
+            ds.merge(ds.dfrnt[u], with: v)
+        }
+
+        if ds.dfrnt[v] == 0 {
+            ds.dfrnt[v] = u
+        } else {
+            ds.merge(ds.dfrnt[v], with: u)
+        }
+    }
+    result += isYes ? "YES\n" : "NO\n"
+}
+
+io.writeByString(result)
