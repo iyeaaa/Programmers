9a0667d713f9288c159a6a83e80b2bab2756d039
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-10 15:37:55 +0900

    [Bronze II] Title: 피로도, Time: 16 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/22864. 피로도/피로도.swift
+++ 백준/Bronze/22864. 피로도/피로도.swift
@@ -5,7 +5,7 @@ let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (A, B, C, M) = (input[0], input[1], input[2], input[3])
 var work = 0
 
-for i in 0..<24 {
+for _ in 0..<24 {
     if fatigue + A > M {
         fatigue -= C
         if fatigue < 0 { fatigue = 0 }
