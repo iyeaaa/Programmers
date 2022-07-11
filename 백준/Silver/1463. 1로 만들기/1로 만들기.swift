9cde2e20e6805c7d04df9b876387f04825174dd9
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-11 17:41:13 +0900

    [Silver III] Title: 1로 만들기, Time: 52 ms, Memory: 76916 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.swift
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.swift
@@ -1,15 +1,14 @@
-let n = Int(readLine()!)!
-var dp = [Int](repeating: 0, count: n+1)
-dp[1] = 0
+let N = Int(readLine()!)!
+var dp = [Int](repeating: Int.max, count: N+1); dp[1] = 0
 
-for i in stride(from: 2, through: n, by: 1) {
+for i in stride(from: 2, through: N, by: 1) {
     dp[i] = dp[i-1] + 1
     if i % 2 == 0 {
-        dp[i] = min(dp[i], dp[i/2] + 1)
+        dp[i] = min(dp[i], dp[i/2]+1)
     }
     if i % 3 == 0 {
-        dp[i] = min(dp[i], dp[i/3] + 1)
+        dp[i] = min(dp[i], dp[i/3]+1)
     }
 }
 
-print(dp[n])
+print(dp[N])
