23df8cde7de68d46200384a588be4b5afdba0b6b
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-12 13:12:09 +0900

    [Silver I] Title: 퇴사 2, Time: 212 ms, Memory: 155324 KB -BaekjoonHub

--- 백준/Silver/15486. 퇴사 2/퇴사 2.swift
+++ 백준/Silver/15486. 퇴사 2/퇴사 2.swift
@@ -1,20 +1,81 @@
-let n = Int(readLine()!)!
-var c = [Consulting(t: 0, p: 0)]
-var dp = [Int](repeating: 0, count: n+1)
+import Foundation
 
-for _ in 0..<n {
-    let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
-    c.append(Consulting(t: input[0], p: input[1]))
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
 }
 
-dp[n] = (c[n].t == 1 ? c[n].p : 0)
+let io = IO()
+typealias Consulting = (T: Int, P: Int)
 
-for i in stride(from: n-1, through: 1, by: -1) {
-    if c[i].t + i <= n+1 {
-        if c[i].t + i == n+1 {
-            dp[i] = max(c[i].p, dp[i+1])
+let N = io.readInt()
+var cst = crtCst()
+var dp = [Int](repeating: 0, count: N)
+dp[N-1] = cst[N-1].T <= 1 ? cst[N-1].P : 0
+
+for i in stride(from: N-2, through: 0, by: -1) {
+    if i+cst[i].T <= N {
+        if i+cst[i].T == N {
+            dp[i] = max(cst[i].P, dp[i+1])
         } else {
-            dp[i] = max(dp[c[i].t + i]+c[i].p, dp[i+1])
+            dp[i] = max(dp[i+cst[i].T]+cst[i].P, dp[i+1])
         }
     } else {
         dp[i] = dp[i+1]
@@ -23,7 +84,10 @@ for i in stride(from: n-1, through: 1, by: -1) {
 
 print(dp.max()!)
 
-struct Consulting {
-    let t: Int
-    let p: Int
+func crtCst() -> [Consulting] {
+    var cst = [Consulting]()
+    for _ in 0..<N {
+        cst.append(Consulting(T: io.readInt(), P: io.readInt()))
+    }
+    return cst
 }
