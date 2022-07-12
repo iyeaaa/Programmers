c12e338163927e4ca798f13ad630150e24f4ef2c
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-12 14:26:17 +0900

    [Silver I] Title: 포도주 시식, Time: 16 ms, Memory: 69264 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.swift
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.swift
@@ -1,28 +1,18 @@
 let n = Int(readLine()!)!
-var list = [Int]()
-var dp = [Int](repeating: 0, count: n)
+var grape = [Int](repeating: 0, count: 10001); crtGrape()
+var dp = [Int](repeating: 0, count: 10001)
+dp[0] = grape[0]
+dp[1] = grape[0] + grape[1]
+dp[2] = max(grape[0] + grape[2], grape[1] + grape[2], grape[0]+grape[1])
 
-for _ in 0..<n {
-    list.append(Int(readLine()!)!)
+for i in stride(from: 3, to: n, by: 1) {
+    dp[i] = max(dp[i-2]+grape[i], dp[i-1], dp[i-3]+grape[i-1]+grape[i])
 }
 
-if n == 1 {
-    print(list[0])
-} else if n == 2 {
-    print(list[0]+list[1])
-} else if n == 3 {
-    print(max(list[0]+list[2], list[0]+list[2], list[0]+list[1]))
-} else {
-    dp[0] = list[0]
-    dp[1] = list[0]+list[1]
-    dp[2] = max(dp[1], list[0]+list[2], list[1]+list[2])
+print(dp.max()!)
 
-    for i in stride(from: 3, to: n, by: 1) {
-        dp[i] = max(dp[i-2]+list[i], dp[i-3]+list[i-1]+list[i], dp[i-1])
+func crtGrape() {
+    for i in 0..<n {
+        grape[i] = Int(readLine()!)!
     }
-
-    print(dp[n-1])
 }
-
-
-
