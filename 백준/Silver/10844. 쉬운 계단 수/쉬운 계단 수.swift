167c84924fc4881af2343dc5760760cca142a333
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-12 16:22:14 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.swift
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.swift
@@ -1,28 +1,13 @@
-var dp = [[Int]](repeating: [Int](repeating: 1, count: 10), count: 101)
-dp[1][0] = 0
+let N = Int(readLine()!)!
+var dp = [[Int]](repeating: [Int](repeating: 0, count: 10), count: 101)
+dp[1] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-for i in 2...100 {
-    for j in 0...9 {
-        if j == 0 {
-            dp[i][j] = dp[i-1][1] % 1_000_000_000
-        } else if j == 9 {
-            dp[i][j] = dp[i-1][8] % 1_000_000_000
-        } else {
-            dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1_000_000_000
-        }
+for i in stride(from: 2, through: N, by: 1) {
+    dp[i][0] = dp[i-1][1] % 1_000_000_000
+    dp[i][9] = dp[i-1][8] % 1_000_000_000
+    for j in 1...8 {
+        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1_000_000_000
     }
 }
 
-var sum = 0
-let n = Int(readLine()!)!
-for i in 0...9 {
-    sum = (sum + dp[n][i]) % 1_000_000_000
-}
-
-print(sum)
-
-/*
- 1, 2, 3, 4, 5, 6, 7, 8, 9
- 10, 12, 21, 23, 32, 34, ..., 89, 98, x
- 101, 121, 123, 210, 212, 232, 234, 321, ..., 987, 989
- */
+print(dp[N].reduce(0, +) % 1_000_000_000)
