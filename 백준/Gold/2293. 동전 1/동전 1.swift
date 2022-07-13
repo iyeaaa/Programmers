e8c5d56defac9574f51a68c1bcc087da986d7852
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-13 13:58:39 +0900

    [Gold V] Title: 동전 1, Time: 32 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.swift
+++ 백준/Gold/2293. 동전 1/동전 1.swift
@@ -2,23 +2,23 @@ import Foundation
 
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (n, k) = (input[0], input[1])
-var coin = [Int]()
-var dp = [Int](repeating: 0, count: k+1)
+let coins = crtCoins()
+let UPPERLIMIT = Int(pow(2, 31.0))
+var dp = [Int](repeating: 0, count: 10001); dp[0] = 1
 
-for _ in 0..<n {
-    coin.append(Int(readLine()!)!)
-}
-
-coin.sort()
-
-dp[0] = 1
-for c in coin {
-    for i in stride(from: c, through: k, by: 1) {
-        if dp[i] + dp[i-c] >= Int(pow(2.0, 31.0)) {
-            dp[i] = 0
-        }
-        dp[i] += dp[i-c]
+for coin in coins {
+    for i in stride(from: coin, through: k, by: 1) {
+        dp[i] = (dp[i] + dp[i-coin]) % UPPERLIMIT
     }
 }
 
 print(dp[k])
+
+
+func crtCoins() -> [Int] {
+    var result = [Int]()
+    for _ in 0..<n {
+        result.append(Int(readLine()!)!)
+    }
+    return result
+}
