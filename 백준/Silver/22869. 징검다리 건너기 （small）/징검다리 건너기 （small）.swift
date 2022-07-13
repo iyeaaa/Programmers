6ed82c20000ceb1c26aaf6d3acfe579eab1f5c88
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-13 12:12:54 +0900

    [Silver I] Title: 징검다리 건너기 (small), Time: 72 ms, Memory: 69520 KB -BaekjoonHub

--- 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.swift
+++ 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.swift
@@ -1,22 +1,14 @@
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (N, K) = (input[0], input[1])
 var A = [0] + readLine()!.split{$0==" "}.map{Int(String($0))!}
+var dp = [Bool](repeating: false, count: 5001); dp[1] = true
 
-var queue = [N], index = 0, success = false
-var visit = [Bool](repeating: false, count: N+1); visit[N] = true
-while index < queue.count {
-    let cur = queue[index]; index += 1
-    if cur == 1 {
-        success = true
-        break
-    }
-    for i in stride(from: cur-1, through: 1, by: -1) {
-        let power = (cur-i)*(1+abs(A[i]-A[cur]))
-        if power <= K && !visit[i] {
-            queue.append(i)
-            visit[i] = true
-        }
+for i in 1...N {
+    if !dp[i] { continue }
+    for j in i...N {
+        if (j-i)*(1+abs(A[i]-A[j])) > K { continue }
+        dp[j] = true
     }
 }
 
-print(success ? "YES" : "NO")
+print(dp[N] ? "YES" : "NO")
