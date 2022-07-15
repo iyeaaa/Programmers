2f1321eca6a9a70f378ffeebdafb2343fed979b1
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-15 17:47:28 +0900

    [Silver III] Title: 서강근육맨, Time: 24 ms, Memory: 70096 KB -BaekjoonHub

--- 백준/Silver/20300. 서강근육맨/서강근육맨.swift
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.swift
@@ -1,15 +1,9 @@
-let n = Int(readLine()!)!
-let input = readLine()!.split{$0==" "}.map{Int(String($0))!}.sorted()
-var result = 0
-if n % 2 == 0 {
-    for i in 0..<n/2 {
-        result = max(result, input[i]+input[n-i-1])
-    }
-} else {
-    result = input[n-1]
-    for i in 0..<(n-1)/2 {
-        result = max(result, input[i]+input[n-i-2])
-    }
+let N = Int(readLine()!)!
+let list = readLine()!.split{$0==" "}.map{Int(String($0))!}.sorted()
+var M = N%2 == 0 ? 0 : list[N-1]
+
+for i in 0..<N/2 {
+    M = max(M, list[i]+list[N-i - (N%2==0 ? 1 : 2)])
 }
 
-print(result)
+print(M)
