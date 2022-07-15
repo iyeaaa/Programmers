97fd98e7e8e44001caca173b366fef127264af4d
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-15 14:55:30 +0900

    [Silver V] Title: 거스름돈, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.swift
+++ 백준/Silver/14916. 거스름돈/거스름돈.swift
@@ -1,14 +1,13 @@
 var n = Int(readLine()!)!
 var count = 0
-while n > 0 {
-    if n % 5 != 0 {
+
+if n == 1 || n == 3 {
+    print(-1)
+} else {
+    while n % 5 != 0 {
         n -= 2
         count += 1
-    } else {
-        count += n/5
-        n = 0
-        break
     }
+    print(count + n/5)
 }
 
-print(n == 0 ? count : -1)
