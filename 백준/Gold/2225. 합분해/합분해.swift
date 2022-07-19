37fc73194460abf3e028dec6a979ea0648ad8187
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-19 17:22:17 +0900

    [Gold V] Title: 합분해, Time: 12 ms, Memory: 69368 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.swift
+++ 백준/Gold/2225. 합분해/합분해.swift
@@ -1,15 +1,15 @@
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
-let (n, k) = (input[0], input[1])
-var dp = [[Int]](repeating: [Int](repeating: 0, count: 201), count: 201)
+let (N, K) = (input[0], input[1])
+var dp = [[Int]](repeating: [Int](repeating: 0, count: K+1), count: N+1)
 
-for i in stride(from: 0, through: k, by: 1) {
-    dp[i][0] = 1
+for i in 0...K {
+    dp[0][i] = 1
 }
 
-for i in stride(from: 1, through: k, by: 1) {
-    for j in stride(from: 1, through: n, by: 1) {
-        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1_000_000_000
+for i in 1...K {
+    for j in 1...N {
+        dp[j][i] = (dp[j-1][i] + dp[j][i-1]) % 1_000_000_000
     }
 }
 
-print(dp[k][n])
+print(dp[N][K])
