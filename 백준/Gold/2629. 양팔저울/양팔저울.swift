d2a13b01b6d90df0af4849851ed87bff7c6b14a1
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-26 16:25:08 +0900

    [Gold III] Title: 양팔저울, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.swift
+++ 백준/Gold/2629. 양팔저울/양팔저울.swift
@@ -2,18 +2,18 @@ let N = Int(readLine()!)!
 let weights = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let M = Int(readLine()!)!
 let beads = readLine()!.split{$0==" "}.map{Int(String($0))!}
-var dp = [Int](repeating: 0, count: 55001); dp[0] = 1
+var isPsb = [Bool](repeating: false, count: 55001); isPsb[0] = true
 
 for weight in weights {
-    for i in stride(from: 15000, through: weight, by: -1) {
-        dp[i] += dp[i-weight]
+    for i in stride(from: 15000, through: weight, by: -1) where isPsb[i-weight] {
+        isPsb[i] = true
     }
 }
 
 var result = ""
 var isYes = false
 for bead in beads {
-    for i in 0...15000 where dp[i] >= 1 && dp[i+bead] >= 1 {
+    for i in 0...15000 where isPsb[i] && isPsb[i+bead] {
         isYes = true
         break
     }
