f992b28056e3eca496f42991eb0ed7263685b571
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-26 12:08:03 +0900

    [Gold IV] Title: 팰린드롬 만들기, Time: 772 ms, Memory: 264696 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.swift
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.swift
@@ -1,11 +1,10 @@
 let N = Int(readLine()!)!
-let list1 = [0] + readLine()!.split{$0==" "}.map{Int(String($0))!}
-let list2 = [0] + list1.reversed()
+let list = [0] + readLine()!.split{$0==" "}.map{Int(String($0))!}
 var dp = [[Int]](repeating: [Int](repeating: 0, count: N+1), count: N+1)
 
-for i in stride(from: 1, through: N, by: 1) {
-    for j in stride(from: 1, through: N, by: 1) {
-        dp[i][j] = list1[i] == list2[j] ? dp[i-1][j-1] + 1 : max(dp[i-1][j], dp[i][j-1])
+for i in 1...N {
+    for j in 1...N {
+        dp[i][j] = list[i] == list[N-j+1] ? dp[i-1][j-1] + 1 : max(dp[i-1][j], dp[i][j-1])
     }
 }
 
