74bd3259fe20ce9ffcb0bc727cf062b789e7344c
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-27 19:35:49 +0900

    [Silver IV] Title: 카드2, Time: 28 ms, Memory: 80824 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.swift
+++ 백준/Silver/2164. 카드2/카드2.swift
@@ -1,30 +1,11 @@
-struct Queue {
-    var queue = [Int]()
-    var index = 0
-
-    init() {}
-    init(_ q: [Int]) { queue = q }
-
-    var count: Int { queue.count - index }
-
-    var first: Int { queue[index] }
-
-    mutating func append(_ q: Int) {
-        queue.append(q)
-    }
-
-    mutating func pop() -> Int {
-        defer { index += 1 }
-        return queue[index]
-    }
-}
-
 let N = Int(readLine()!)!
-var queue = Queue(Array(1...N))
+var queue = Array(1...N)
+var idx = 0
 
-while queue.count > 1 {
-    _ = queue.pop()
-    queue.append(queue.pop())
+while queue.count - idx > 1 {
+    idx += 1
+    queue.append(queue[idx])
+    idx += 1
 }
 
-print(queue.first)
+print(queue[idx])
