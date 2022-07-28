acb6ee975a2c096a7618d0c4346b148d4e85d297
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-28 12:56:10 +0900

    [Silver III] Title: 후위 표기식2, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.swift
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.swift
@@ -1,51 +1,30 @@
 import Foundation
 
-var buffer = Array(FileHandle.standardInput.readDataToEndOfFile())+[0], idx = 0
-@inline(__always) func readByte() -> UInt8 {
-    defer { idx += 1 }
-    return buffer[idx]
-}
-@inline(__always) func readString() -> String {
-    var result = "", byte = readByte()
-    while byte == 10 || byte == 32 { byte = readByte() }
-    while byte != 10 && byte != 32 && byte != 0 {
-        result += String(bytes: [byte], encoding: .ascii)!
-        byte = readByte()
+extension Character {
+    var index: Int {
+        Int(unicodeScalars.first!.value)-65
     }
-    return result
-}
-@inline(__always) func readInt() -> Int {
-    var number = 0, byte = readByte()
-    while byte == 10 || byte == 32 { byte = readByte() }
-    while 48...57 ~= byte { number = number*10 + Int(byte-48); byte = readByte() }
-    return number
 }
 
-let n = readInt()
+let N = Int(readLine()!)!
+let strn = Array(readLine()!)
+var list = [Double](); for _ in 0..<N { list.append(Double(readLine()!)!) }
+
 var stack = [Double]()
-var dict = [Character: Double]()
-for s in readString() {
-    if 65...90 ~= s.unicodeScalars.first!.value {
-        if let d = dict[s] {
-            stack.append(d)
-        } else {
-            let temp = Double(readInt())
-            stack.append(temp)
-            dict[s] = temp
-        }
-    } else {
-        var push = 0.0
-        if s == "+" {
-            push = stack.popLast()! + stack.popLast()!
-        } else if s == "-" {
-            push = -1*(stack.popLast()! - stack.popLast()!)
-        } else if s == "*" {
-            push = stack.popLast()! * stack.popLast()!
-        } else {
-            let temp = stack.popLast()!
-            push = stack.popLast()! / temp
-        }
-        stack.append(push)
+for c in strn {
+    switch c {
+    case "+":
+        stack.append(stack.popLast()!+stack.popLast()!)
+    case "-":
+        let (x, y) = (stack.popLast()!, stack.popLast()!)
+        stack.append(y-x)
+    case "*":
+        stack.append(stack.popLast()!*stack.popLast()!)
+    case "/":
+        let (x, y) = (stack.popLast()!, stack.popLast()!)
+        stack.append(y/x)
+    default:
+        stack.append(list[c.index])
     }
 }
 
