c692b1488c0f8f1f324cedd009c83aea6103f4c8
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-28 18:45:22 +0900

    [Silver I] Title: 단지번호붙이기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
@@ -1,53 +1,45 @@
-let n = Int(readLine()!)!
-var node = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
 
-mapConstructor()
-printResult()
 
-func mapConstructor() {
-    for i in 0..<n {
-        let temp = Array(readLine()!)
-        for j in 0..<n {
-            node[i][j] = Int(String(temp[j]))!
+let N = Int(readLine()!)!
+let graph: [[Int]] = crtGraph()
+var visit = [[Bool]](repeating: [Bool](repeating: false, count: N), count: N)
+
+var count = [Int]()
+for i in 0..<N {
+    for j in 0..<N {
+        if graph[i][j] == 1 && !visit[i][j] {
+            count.append(bfs(i, j))
         }
     }
 }
 
-func printResult() {
-    var result = [Int]()
-    for i in 0..<n {
-        for j in 0..<n {
-            if node[i][j] == 1 {
-                result.append(bfs(i, j))
+count.sort()
+var result = "\(count.count)\n"
+for c in count {
+    result += "\(c)\n"
+}
+print(result)
+
+func bfs(_ y: Int, _ x: Int) -> Int {
+    var count = 1
+    var queue = [(y, x)], idx = 0; visit[y][x] = true
+    while idx < queue.count {
+        let (y, x) = queue[idx]; idx += 1
+        for (ny, nx) in [(y-1, x), (y+1, x), (y, x+1), (y, x-1)] {
+            if (0..<N) ~= ny && (0..<N) ~= nx && !visit[ny][nx] && graph[ny][nx] == 1 {
+                queue.append((ny, nx))
+                visit[ny][nx] = true
+                count += 1
             }
         }
     }
-    var re = "\(result.count)\n"
-    result.sort()
-    for i in result {
-        re += "\(i)\n"
-    }
-    print(re)
+    return count
 }
 
-func bfs(_ x: Int, _ y: Int) -> Int {
-    let dx = [-1, 0, 1, 0]
-    let dy = [0, -1, 0, 1]
-    node[x][y] = 0
-    var index = 0
-    var queue = [[x, y]]
-    while index < queue.count {
-        for i in 0...3 {
-            let nx = queue[index][0] + dx[i]
-            let ny = queue[index][1] + dy[i]
-            if nx < 0 || nx > n-1 || ny < 0 || ny > n-1 || node[nx][ny] == 0 {
-                continue
-            } else {
-                queue.append([nx, ny])
-                node[nx][ny] = 0
-            }
-        }
-        index += 1
+func crtGraph() -> [[Int]] {
+    var result = [[Int]]()
+    for _ in 0..<N {
+        result.append(readLine()!.map{Int(String($0))!})
     }
-    return queue.count
+    return result
 }
