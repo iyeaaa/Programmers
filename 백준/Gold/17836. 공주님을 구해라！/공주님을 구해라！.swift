10db0943700749b888e8e4765b2fdb55717d5fdb
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-29 20:56:06 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 12 ms, Memory: 69408 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.swift
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.swift
@@ -1,69 +1,30 @@
-import Foundation
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (N ,M, T) = (input[0], input[1], input[2])
+let graph: [[Int]] = crtGraph()
 
-var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), bufIdx = 0
-buffer.append(0)
+var visit = [[Int]](repeating: [Int](repeating: -1, count: M), count: N)
+var queue = [(0, 0)], idx = 0; visit[0][0] = 0
+var result = Int.max
 
-@inline(__always) func readByte() -> UInt8 {
-    defer { bufIdx += 1 }
-    return buffer[bufIdx]
-}
-
-@inline(__always) func readInt() -> Int {
-    var number = 0, byte = readByte(), isNegative = false
-    while byte == 10 || byte == 32 { byte = readByte() }
-    if byte == 45 {byte = readByte(); isNegative = true }
-    while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
-    return number * (isNegative ? -1 : 1)
-}
-
-struct C {
-    let x: Int
-    let y: Int
-    let weapon: Int
-}
-
-let (n, m, t) = (readInt(), readInt(), readInt())
-var graph = Array(repeating: Array(repeating: 0, count: m), count: n)
-var visit = Array(repeating: Array(repeating: Array(repeating: -1, count: 2), count: m), count: n)
-let direction = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-
-(0..<n).forEach { i in
-    (0..<m).forEach { j in
-        graph[i][j] = readInt()
+while idx < queue.count {
+    let (y, x) = queue[idx]; idx += 1
+    if y == N-1 && x == M-1 { result = min(visit[N-1][M-1], result); break }
+    if graph[y][x] == 2 { result = min(visit[y][x] + N-1-y + M-1-x, result) }
+    for (ny, nx) in [(y-1,x),(y+1,x),(y,x+1),(y,x-1)] {
+        if !((0..<N) ~= ny && (0..<M) ~= nx) { continue }
+        if visit[ny][nx] != -1 || graph[ny][nx] == 1 { continue }
+        queue.append((ny, nx))
+        visit[ny][nx] = visit[y][x] + 1
     }
 }
+print(result > T ? "Fail" : result)
 
-var queue = [C(x: 0, y: 0, weapon: 0)]
-visit[0][0][0] = 0
-var index = 0
-var result = -1
-while index < queue.count {
-    let cur = queue[index]
-    if cur.x == n-1 && cur.y == m-1 {
-        let temp = visit[n-1][m-1][cur.weapon]
-        result = temp <= t ? temp : -1
-        break
-    }
-    for d in direction {
-        let nx = (x: cur.x + d[0], y: cur.y + d[1])
-        if !(0..<n).contains(nx.x) || !(0..<m).contains(nx.y) {
-            continue
-        }
-        if visit[nx.x][nx.y][cur.weapon] != -1 {
-            continue
-        }
-        if (graph[nx.x][nx.y] == 1 && cur.weapon == 1)
-                || graph[nx.x][nx.y] == 2 {
-            queue.append(C(x: nx.x, y: nx.y, weapon: 1))
-            visit[nx.x][nx.y][1] = visit[cur.x][cur.y][cur.weapon] + 1
-        }
 
-        if graph[nx.x][nx.y] == 0 {
-            queue.append(C(x: nx.x, y: nx.y, weapon: cur.weapon))
-            visit[nx.x][nx.y][cur.weapon] = visit[cur.x][cur.y][cur.weapon] + 1
-        }
+func crtGraph() -> [[Int]] {
+    var result = [[Int]]()
+    for _ in 0..<N {
+        result.append(readLine()!.split(separator: " ").map{Int(String($0))!})
     }
-    index += 1
+    return result
 }
 
-print(result == -1 ? "Fail" : result)
