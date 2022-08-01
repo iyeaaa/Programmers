8248dac2ae0938a52f699289fb8ec6f386f3787e
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-08-01 12:57:06 +0900

    [Gold IV] Title: 불!, Time: 348 ms, Memory: 158308 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.swift
+++ 백준/Gold/4179. 불！/불！.swift
@@ -1,94 +1,70 @@
-import Foundation
+let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+let (R, C) = (input[0], input[1])
+let graph: [[Character]] = crtGraph()
+var fireStart = [[Int]]()
+var fireSecond = [[Int]](repeating: [Int](repeating: Int.max, count: C), count: R)
+var jihun = [Int]()
 
-var buffer = Array(FileHandle.standardInput.readDataToEndOfFile()), idx = 0
-buffer.append(0)
-@inline(__always) func readByte() -> UInt8 {
-    defer { idx += 1 }
-    return buffer[idx]
-}
-@inline(__always) func readInt() -> Int {
-    var number = 0, byte = readByte()
-    while byte == 10 || byte == 32 { byte = readByte() }
-    while 48...57 ~= byte { number = number * 10 + Int(byte-48); byte = readByte()}
-    return number
-}
-@inline(__always) func readChar() -> Character {
-    var byte = readByte()
-    while byte == 10 || byte == 32 { byte = readByte() }
-    return Character(UnicodeScalar(byte))
-}
-
-let (r, c) = (readInt(), readInt())
-var graph = Array(repeating: Array(repeating: Character("."), count: c), count: r)
-var visit = Array(repeating: Array(repeating: -1, count: c), count: r)
-var fire = Array(repeating: Array(repeating: -1, count: c), count: r)
-var jihun = [Int](), firstFire = [[Int]]()
-let direction = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-var fireEA = 0
-(0..<r).forEach { i in
-    (0..<c).forEach { j in
-        graph[i][j] = readChar()
+for i in 0..<R {
+    for j in 0..<C {
         if graph[i][j] == "F" {
-            firstFire.append([i, j])
-            fireEA += 1
+            fireStart.append([i, j])
+            continue
         }
         if graph[i][j] == "J" {
             jihun = [i, j]
-        }
-    }
-}
-
-if fireEA != 0 {
-    moveFire()
-}
-moveJihun()
-
-print("IMPOSSIBLE")
-
-func isIn(_ y: Int, _ x: Int) -> Bool {
-    0..<r ~= y && 0..<c ~= x
-}
-
-func moveFire() {
-    var index = 0, queue = firstFire
-    fire[firstFire[0][0]][firstFire[0][1]] = 0
-    while index < queue.count {
-        let (y, x) = (queue[index][0], queue[index][1])
-        for d in direction {
-            let (ny, nx) = (y+d[0], x+d[1])
-            if !isIn(ny, nx) || fire[ny][nx] != -1 {
-                continue
-            }
-            if graph[ny][nx] == "#" {
-                continue
-            }
-            fire[ny][nx] = fire[y][x] + 1
-            queue.append([ny, nx])
-        }
-        index += 1
-    }
-}
-
-func moveJihun() {
-    var index = 0, queue = [jihun]
-    visit[jihun[0]][jihun[1]] = 0
-    while index < queue.count {
-        let (y, x) = (queue[index][0], queue[index][1])
-        if fire[y][x] != -1 && visit[y][x] >= fire[y][x] {
-            index += 1
             continue
         }
-        for d in direction {
-            let (ny, nx) = (y+d[0], x+d[1])
-            if !isIn(ny, nx) {
-                print(visit[y][x] + 1); exit(0)
-            }
-            if visit[ny][nx] != -1 || graph[ny][nx] == "#" {
-                continue
-            }
-            visit[ny][nx] = visit[y][x] + 1
-            queue.append([ny, nx])
-        }
-        index += 1
     }
 }
+
+var result: Any = "IMPOSSIBLE"
+if !fireStart.isEmpty {
+    fireSpread()
+}
+print(canEscape())
+
+
+func canEscape() -> Any {
+    var visit = [[Int]](repeating: [Int](repeating: -1, count: C), count: R)
+    var queue = [jihun], idx = 0; visit[jihun[0]][jihun[1]] = 0
+
+    while idx < queue.count {
+        let (y, x) = (queue[idx][0], queue[idx][1]); idx += 1
+        if y == R-1 || x == C-1 || y == 0 || x == 0 { return visit[y][x]+1 }
+        for (ny, nx) in [(y+1,x),(y-1,x),(y,x+1),(y,x-1)] {
+            if visit[ny][nx] != -1 { continue }
+            if graph[ny][nx] == "#" { continue }
+            if fireSecond[ny][nx] <= visit[y][x]+1 { continue }
+            queue.append([ny, nx])
+            visit[ny][nx] = visit[y][x] + 1
+        }
+    }
+
+    return "IMPOSSIBLE"
+}
+
+func fireSpread() {
+    var queue = fireStart, idx = 0
+    for q in fireStart { fireSecond[q[0]][q[1]] = 0 }
+
+    while idx < queue.count {
+        let (y, x) = (queue[idx][0], queue[idx][1]); idx += 1
+        for (ny, nx) in [(y-1,x),(y+1,x),(y,x+1),(y,x-1)] {
+            if !((0..<R) ~= ny && (0..<C) ~= nx) { continue }
+            if fireSecond[ny][nx] != Int.max { continue }
+            if graph[ny][nx] == "#" { continue }
+            queue.append([ny, nx])
+            fireSecond[ny][nx] = fireSecond[y][x] + 1
+        }
+    }
+}
+
+func crtGraph() -> [[Character]] {
+    var result = [[Character]]()
+    for _ in 0..<R {
+        result.append(Array(readLine()!))
+    }
+    return result
+}
+
