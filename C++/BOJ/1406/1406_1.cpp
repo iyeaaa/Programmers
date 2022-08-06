d8f3f8f4ef9b8584298c3895e6200967f5620287
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-08-06 13:28:53 +0900

    BOJ-1406 '에디터' [ 22/08/06 13:28:53 ] by Alub

---
+++ C++/BOJ/1406/1406_1.cpp
@@ -0,0 +1,48 @@
+/**
+* 메모리: 21020 KB, 시간: 72 ms
+* 2022.08.06
+* by Alub
+*/
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string strn; cin >> strn;
+    list<char> editor(strn.begin(), strn.end());
+    auto cursor = editor.end();
+    int M; cin >> M;
+
+    while(M--) {
+        char cmd; cin >> cmd;
+        switch (cmd) {
+            case 'L':
+                if(cursor != editor.begin())
+                    cursor--;
+                break;
+            case 'D':
+                if(cursor != editor.end())
+                    cursor++;
+                break;
+            case 'B':
+                if(cursor != editor.begin())
+                    cursor = editor.erase(--cursor);
+                break;
+            case 'P':
+                char x; cin >> x;
+                editor.insert(cursor, x);
+                break;
+        }
+    }
+
+    for(auto it = editor.begin(); it != editor.end(); it++)
+        cout << *it;
+
+    return 0;
+}
+
+
