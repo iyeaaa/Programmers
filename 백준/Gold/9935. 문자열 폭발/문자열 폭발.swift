60e66d4a5183820426a8e9ee8ca957b02a14e847
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-08-07 14:01:09 +0900

    [Gold IV] Title: 문자열 폭발, Time: 316 ms, Memory: 117508 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.swift
@@ -0,0 +1,16 @@
+let strn = Array(readLine()!).map{String($0)}
+let bomb = readLine()!, bombCnt = bomb.count, bombLast = String(bomb.last!)
+var stack = [String]()
+
+for c in strn {
+    stack.append(c)
+    let stackCnt = stack.count
+    if stack.last! != bombLast { continue }
+    if stack.count >= bomb.count && stack[(stackCnt-bombCnt)...].joined() == bomb {
+        for _ in 0..<bombCnt {
+            stack.removeLast()
+        }
+    }
+}
+
+print(stack.isEmpty ? "FRULA" : stack.joined())
