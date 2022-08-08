43d9015a863aad5698eedd11f52e4f241d844841
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-08-08 11:29:55 +0900

    [Silver IV] Title: 좋은 단어, Time: 32 ms, Memory: 70768 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.swift
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.swift
@@ -1,20 +1,13 @@
 let N = Int(readLine()!)!
-
 var count = 0
 for _ in 0..<N {
     var stack = [Character]()
     let strn = Array(readLine()!)
-    var success = true
-
     for c in strn {
-        if strn.count % 2 == 1 { success = false; break }
         if stack.isEmpty { stack.append(c); continue }
         if stack.last! == c { stack.removeLast(); continue }
         stack.append(c)
     }
-
-    if stack.isEmpty && success {
-        count += 1
-    }
+    count += stack.isEmpty ? 1 : 0
 }
 print(count)
