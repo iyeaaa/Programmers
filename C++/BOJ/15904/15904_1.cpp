63072db9fbb15865866a53edf8e8f2627fb04a62
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-08-16 22:56:06 +0900

    BOJ-15904 'UCPC는 무엇의 약자일까?' [ 22/08/16 22:56:06 ] by Alub

---
+++ C++/BOJ/15904/15904_1.cpp
@@ -0,0 +1,31 @@
+/**
+* 메모리: 2024 KB, 시간: 0 ms
+* 2022.08.16
+* by Alub
+*/
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    string strn;
+    getline(cin, strn);
+
+    char ucpc[] = {'U', 'C', 'P', 'C'};
+
+    int idx = 0;
+    for (auto c: strn)
+        if (c == ucpc[idx]) {
+            idx++;
+            if (idx == 4) {
+                cout << "I love UCPC" << endl;
+                return 0;
+            }
+        }
+
+    cout << "I hate UCPC" << endl;
+}
