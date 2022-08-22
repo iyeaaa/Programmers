d623d2f7a89f8b32d47901928fa15b84ea60fc6a
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-08-22 15:29:27 +0900

    BOJ-1448 '삼각형 만들기' [ 22/08/22 15:29:26 ] by Alub

---
+++ C++/BOJ/1448/1448_1.cpp
@@ -0,0 +1,31 @@
+/**
+* 메모리: 5804 KB, 시간: 160 ms
+* 2022.08.22
+* by Alub
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool isPsb(int x, int y, int z) {
+    return x < y+z;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int N, list[1000001], answer = -1;
+    cin >> N;
+    for(int i=0;i<N;i++) cin >> list[i];
+    sort(list, list+N, greater<int>());
+
+    for(int i = 0; i < N-2; i++)
+        if (isPsb(list[i], list[i+1], list[i+2])) {
+            answer = list[i] + list[i + 1] + list[i + 2];
+            break;
+        }
+
+    cout << answer;
+}
