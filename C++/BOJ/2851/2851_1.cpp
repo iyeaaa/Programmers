12ffe724b3fff5803474dc748096d0c744349394
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-09-07 12:27:21 +0900

    BOJ-2851 '슈퍼 마리오' [ 22/09/07 12:27:21 ] by Alub

---
+++ C++/BOJ/2851/2851_1.cpp
@@ -0,0 +1,28 @@
+/**
+* 메모리: 2020 KB, 시간: 0 ms
+* 2022.09.07
+* by Alub
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int L[10];
+    int ans = 0;
+    int sum = 0;
+
+    for (int i = 0; i < 10; i++)
+        cin >> L[i];
+
+    for (int i = 0; i < 10; i++) {
+        sum += L[i];
+        if (abs(ans-100) >= abs(sum-100))
+            ans = sum;
+    }
+
+    cout << ans;
+}
