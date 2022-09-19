2b2385aef1265c521268201bbd5421b8b2c59b8e
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-09-20 00:23:13 +0900

    BOJ-1874 '스택 수열' [ 22/09/20 00:23:13 ] by Alub

---
+++ C++/BOJ/1874/1874_1.swift
@@ -0,0 +1,90 @@
+/**
+* 메모리: 81508 KB, 시간: 44 ms
+* 2022.09.20
+* by Alub
+*/
+import Foundation
+
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+let io = IO()
+let n = io.readInt()
+var stack = [Int]()
+var next_push = 1
+var result = ""
+
+for _ in 0..<n {
+    let x = io.readInt()
+    if x >= next_push {
+        result += String(repeating: "+\n", count: x-next_push+1) + "-\n"
+        stack += Array(next_push..<x)
+        next_push = x+1
+    } else if stack.isEmpty || stack.last! != x {
+        print("NO"); exit(0)
+    } else {
+        result += "-\n"
+        stack.removeLast()
+    }
+}
+
+print(result)
