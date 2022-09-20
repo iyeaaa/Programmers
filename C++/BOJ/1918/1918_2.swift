d564d540d521c5c5ff0aec7c9b5b7a7dbfdbf93d
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-09-20 10:14:05 +0900

    BOJ-1918 '후위 표기식' [ 22/09/20 10:14:05 ] by Alub

---
+++ C++/BOJ/1918/1918_2.swift
@@ -0,0 +1,38 @@
+/**
+* 메모리: 69100 KB, 시간: 8 ms
+* 2022.09.20
+* by Alub
+*/
+func rank(_ x: Character) -> Int {
+    if x == "(" { return 3 }
+    if x == "+" || x == "-" { return 2 }
+    if x == "*" || x == "/" { return 1 }
+    return 0
+}
+
+var stack = [Character]()
+var ans = ""
+
+for x in readLine()! {
+    if x <= "Z" && x >= "A" {
+        ans.append(x)
+    } else if x == ")" {
+        while !stack.isEmpty && stack.last! != "(" {
+            ans.append(stack.popLast()!)
+        }
+        stack.removeLast()
+    } else if x == "(" {
+        stack.append(x)
+    } else {
+        while !stack.isEmpty && rank(stack.last!) <= rank(x) {
+            ans.append(stack.popLast()!)
+        }
+        stack.append(x)
+    }
+}
+
+while !stack.isEmpty {
+    ans.append(stack.popLast()!)
+}
+
+print(ans)
