11ca1082cea465bc0ac7c8224b65af4bf226a82e
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-09-27 10:44:57 +0900

    BOJ-14501 '퇴사' [ 22/09/27 10:44:57 ] by Alub

---
+++ C++/BOJ/14501/14501_1.swift
@@ -0,0 +1,25 @@
+/**
+* 메모리: 69104 KB, 시간: 8 ms
+* 2022.09.27
+* by Alub
+*/
+let N = Int(readLine()!)!
+var T = [Int](repeating: 0, count: N+1)
+var P = [Int](repeating: 0, count: N+1)
+var ans = Int.min
+
+for i in 1...N {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    T[i] = input[0]; P[i] = input[1]
+}
+
+C(1, 0)
+print(ans)
+
+func C(_ idx: Int, _ money: Int) {
+    ans = max(ans, money)
+
+    for i in stride(from: idx, through: N, by: 1) where i+T[i] <= N+1 {
+        C(i+T[i], money+P[i])
+    }
+}
