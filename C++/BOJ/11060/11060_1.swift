65e1250f73ba6a562e120e511d4e88916ebd07e3
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-09-30 16:20:34 +0900

    BOJ-11060 '점프 점프' [ 22/09/30 16:20:34 ] by Alub

---
+++ C++/BOJ/11060/11060_1.swift
@@ -0,0 +1,16 @@
+/**
+* 메모리: 69104 KB, 시간: 12 ms
+* 2022.09.30
+* by Alub
+*/
+let N = Int(readLine()!)!
+let M = readLine()!.split(separator: " ").map{Int(String($0))!}
+var memo = [Int](repeating: 999999999, count: N); memo[0] = 0
+
+for i in 1..<N {
+    for j in 0..<i where j + M[j] >= i {
+        memo[i] = min(memo[i], memo[j]+1)
+    }
+}
+
+print(memo[N-1] == 999999999 ? -1 : memo[N-1])
