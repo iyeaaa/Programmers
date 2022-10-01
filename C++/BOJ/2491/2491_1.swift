538962f6ef4277d17e512766e0a7752d0e8fe699
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-10-01 20:25:16 +0900

    BOJ-2491 '수열' [ 22/10/01 20:25:16 ] by Alub

---
+++ C++/BOJ/2491/2491_1.swift
@@ -0,0 +1,17 @@
+/**
+* 메모리: 76876 KB, 시간: 52 ms
+* 2022.10.01
+* by Alub
+*/
+let N = Int(readLine()!)!
+let S = readLine()!.split(separator: " ").map{Int(String($0))!}
+var memo = [[Int]](repeating: [Int](repeating: 1, count: 2), count: N)
+var ans = 1
+
+for i in 1..<N {
+    memo[i][0] = S[i-1] <= S[i] ? memo[i-1][0]+1 : 1
+    memo[i][1] = S[i-1] >= S[i] ? memo[i-1][1]+1 : 1
+    ans = max(ans, memo[i].max()!)
+}
+
+print(ans)
