e67155045d28e2f7bced9efc3eea0666d9072749
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-10-10 22:00:52 +0900

    BOJ-2482 '색상환' [ 22/10/10 22:00:52 ] by Alub

---
+++ C++/BOJ/2482/2482_1.swift
@@ -0,0 +1,18 @@
+/**
+* 메모리: 69100 KB, 시간: 8 ms
+* 2022.10.10
+* by Alub
+*/
+func C(_ n: Int, _ k: Int) -> Int {
+    var d = [Int](repeating: 0, count: 1005); d[0] = 1
+    for i in 0...n {
+        for j in stride(from: min(i, k), through: 1, by: -1) {
+            d[j] = (d[j-1] + d[j]) % MOD
+        }
+    }
+    return d[k]
+}
+
+let MOD = 1_000_000_003
+let (N, K) = (Int(readLine()!)!, Int(readLine()!)!)
+print(K == 1 ? N : (MOD+C(N-K+1, K)-C(N-K-1, K-2)) % MOD)
