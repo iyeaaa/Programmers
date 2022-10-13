52828a0e7b81290ce090c45080e4710982bd1b46
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-10-13 20:01:00 +0900

    BOJ-17202 '핸드폰 번호 궁합' [ 22/10/13 20:01:00 ] by Alub

---
+++ C++/BOJ/17202/17202_1.swift
@@ -0,0 +1,21 @@
+/**
+* 메모리: 69104 KB, 시간: 8 ms
+* 2022.10.13
+* by Alub
+*/
+let A = Array(readLine()!).map{Int(String($0))!}
+let B = Array(readLine()!).map{Int(String($0))!}
+var L = [Int]()
+
+for i in 0..<8 {
+    L.append(A[i])
+    L.append(B[i])
+}
+
+for i in stride(from: 16, through: 3, by: -1) {
+    for j in 0..<i-1 {
+        L[j] = (L[j] + L[j+1]) % 10
+    }
+}
+
+print(String(L[0]) + String(L[1]))
