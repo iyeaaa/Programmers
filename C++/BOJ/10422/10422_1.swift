4e5fa19d024a135421824a52b6fb08e68f70f044
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-10-19 14:39:09 +0900

    BOJ-10422 '괄호' [ 22/10/19 14:39:08 ] by Alub

---
+++ C++/BOJ/10422/10422_1.swift
@@ -0,0 +1,20 @@
+/**
+* 메모리: 69100 KB, 시간: 24 ms
+* 2022.10.19
+* by Alub
+*/
+let MOD = 1_000_000_007
+var memo = (0...5000).map{_ in 0}
+memo[0] = 1; memo[2] = 1
+
+for i in stride(from: 4, through: 5000, by: 2) {
+    var temp = 0
+    for j in stride(from: 2, through: i, by: 2) {
+        temp = (temp + memo[j-2]*memo[i-j]) % MOD
+    }
+    memo[i] = temp
+}
+
+for _ in 0..<Int(readLine()!)! {
+    print(memo[Int(readLine()!)!])
+}
