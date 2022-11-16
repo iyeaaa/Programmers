a092f764ad69a306dbedd1061f8d61e539d15298
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-11-16 17:07:30 +0900

    [Gold II] Title: 공항, Time: 64 ms, Memory: 72112 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.swift
+++ 백준/Gold/10775. 공항/공항.swift
@@ -1,29 +1,40 @@
+
+struct Disjoint_Set {
+    var parent: [Int]
+    var rank: [Int]
+
+    init(_ x: Int) {
+        parent = Array(0..<x)
+        rank = [Int](repeating: 0, count: x)
+    }
+
+    mutating func root(_ x: Int) -> Int {
+        if parent[x] == x { return x }
+        parent[x] = root(parent[x])
+        return parent[x]
+    }
+
+    mutating func uni(_ x: Int, _ y: Int) {
+        let x = root(x), y = root(y)
+        if rank[x] > rank[y] { parent[y] = x }
+        else { parent[x] = y }
+        if rank[x] == rank[y] { rank[y] += 1 }
+    }
+
+    mutating func isUni(_ x: Int, _ y: Int) -> Bool {
+        root(x) == root(y)
+    }
+}
+
 let G = Int(readLine()!)!
 let P = Int(readLine()!)!
-var gate = Array(0...G)
+var uf = Disjoint_Set(G+1)
+var ans = 0
 
-var result = 0
 for _ in 0..<P {
-    let docking = root(of: Int(readLine()!)!)
-    if root(of: docking) == 0 {
-        break
-    } else {
-        merge(docking, with: docking-1)
-        result += 1
-    }
+    let g = uf.root(Int(readLine()!)!)
+    if uf.root(g) == 0 { break }
+    else { uf.parent[g] = g-1; ans += 1 }
 }
 
-print(result)
-
-func root(of x : Int) -> Int {
-    if x == gate[x] {
-        return x
-    }
-    gate[x] = root(of: gate[x])
-    return gate[x]
-}
-func merge(_ x: Int, with y: Int) {
-    let (x, y) = (root(of: x), root(of: y))
-    if x == y { return }
-    gate[x] = y
-}
+print(ans)
