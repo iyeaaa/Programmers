2ca2f118b26513a8608ca2c13b7dbf8f1c23b5ec
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-11-28 23:01:49 +0900

    [Gold IV] Title: 흩날리는 시험지 속에서 내 평점이 느껴진거야, Time: 40 ms, Memory: 75696 KB -BaekjoonHub

--- 백준/Gold/17951. 흩날리는 시험지 속에서 내 평점이 느껴진거야/흩날리는 시험지 속에서 내 평점이 느껴진거야.swift
+++ 백준/Gold/17951. 흩날리는 시험지 속에서 내 평점이 느껴진거야/흩날리는 시험지 속에서 내 평점이 느껴진거야.swift
@@ -1,8 +1,7 @@
-
 let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
 let (N, K) = (input[0], input[1])
 let x = readLine()!.split{$0==" "}.map{Int(String($0))!}
-var lo = -1, hi = 20*100000+1
+var lo = -1, hi = 20*N+1
 while lo+1 < hi {
     let mid = (lo+hi)/2
     var cnt = 0, sum = 0
