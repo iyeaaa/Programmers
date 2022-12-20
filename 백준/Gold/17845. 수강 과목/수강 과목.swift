7ca1857f76a10ea9048e0a4e27c1630e7b43d009
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-12-20 16:55:42 +0900

    [Gold V] Title: 수강 과목, Time: 148 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.swift
+++ 백준/Gold/17845. 수강 과목/수강 과목.swift
@@ -1,85 +1,14 @@
 
-import Foundation
-
-final class IO {
-    private let buffer:[UInt8]
-    private var index: Int = 0
-
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-
-        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
-    }
-
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-
-        return buffer[index]
-    }
-
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-
-        while now == 10
-                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
-        }
-
-        return sum * (isPositive ? 1:-1)
-    }
-
-    @inline(__always) func readString() -> String {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
-    }
-
-    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
-        var now = read()
-
-        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
-        let beginIndex = index-1
-
-        while now != 10,
-              now != 32,
-              now != 0 { now = read() }
-
-        return Array(buffer[beginIndex..<(index-1)])
-    }
-
-    @inline(__always) func writeByString(_ output: String) { // wapas
-        FileHandle.standardOutput.write(output.data(using: .utf8)!)
-    }
-}
-
-let INF = -99999999999
-let io = IO()
-let n = io.readInt(), k = io.readInt()
-var o = [(l:0, t:0)]
-var dp = [[Int]](repeating: [Int](repeating: INF, count: n+1), count: k+1)
-dp[0][0] = 0
+let ip = readLine()!.split{$0==" "}.map{Int(String($0))!}
+let (n, k) = (ip[0], ip[1])
+var dp = [Int](repeating: Int.min, count: 10001); dp[0] = 0
 
 for _ in 0..<k {
-    o.append((io.readInt(), io.readInt()))
-}
-
-for i in 1...k {
-    let (l, t) = o[i]
-    dp[i] = dp[i-1]
-    for j in stride(from: t, through: n, by: 1) {
-        dp[i][j] = max(dp[i][j], l + dp[i-1][j-t])
+    let ip = readLine()!.split{$0==" "}.map{Int(String($0))!}
+    let (i, t) = (ip[0], ip[1])
+    for time in stride(from: n, through: t, by: -1) {
+        dp[time] = max(dp[time], dp[time-t] + i)
     }
 }
 
-print(dp[k].max()!)
+print(dp.max()!)
