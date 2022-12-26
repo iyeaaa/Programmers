14f868983b49c552e4291d6905b0e2cb8976acb9
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-12-26 19:26:34 +0900

    [Gold V] Title: 풍선 맞추기, Time: 64 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.cc
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, x, cnt = 0;
+int isArrow[1000001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> x;
+        if (!isArrow[x]) {
+            isArrow[x-1]++;
+            cnt++;
+        } else {
+            isArrow[x]--;
+            isArrow[x-1]++;
+        }
+    }
+
+    cout << cnt;
+}
+
