8029a4d36a1f8b692be423fd3c6ad8e5715c17ad
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-12-31 21:23:20 +0900

    [Gold III] Title: 200년간 폐관수련했더니 PS 최강자가 된 건에 대하여, Time: 44 ms, Memory: 85764 KB -BaekjoonHub

--- 백준/Gold/19582. 200년간 폐관수련했더니 PS 최강자가 된 건에 대하여/200년간 폐관수련했더니 PS 최강자가 된 건에 대하여.swift
+++ 백준/Gold/19582. 200년간 폐관수련했더니 PS 최강자가 된 건에 대하여/200년간 폐관수련했더니 PS 최강자가 된 건에 대하여.swift
@@ -1,13 +1,61 @@
 
 import Foundation
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
 
-let N = Int(readLine()!)!
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        while now != 10
+                      && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+
+        return str
+    }
+}
+
+
+let file = FileIO()
+let N = file.readInt()
 var sum = 0, mx = 0, removed = false, ans = "Kkeo-eok"
 var C = [(x: Int, p: Int)]()
 
 for _ in 0..<N {
-    let ip = readLine()!.split{$0==" "}.map{Int(String($0))!}
-    C.append((ip[0], ip[1]))
+    C.append((file.readInt(), file.readInt()))
 }
 
 for (x, p) in C {
