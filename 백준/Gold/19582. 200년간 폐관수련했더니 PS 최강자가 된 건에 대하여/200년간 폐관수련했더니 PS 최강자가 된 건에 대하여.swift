cfcadeb84004225f1169b383e8a4706bf22c0328
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-12-31 21:21:48 +0900

    [Gold III] Title: 200년간 폐관수련했더니 PS 최강자가 된 건에 대하여, Time: 184 ms, Memory: 82640 KB -BaekjoonHub

--- 백준/Gold/19582. 200년간 폐관수련했더니 PS 최강자가 된 건에 대하여/200년간 폐관수련했더니 PS 최강자가 된 건에 대하여.swift
+++ 백준/Gold/19582. 200년간 폐관수련했더니 PS 최강자가 된 건에 대하여/200년간 폐관수련했더니 PS 최강자가 된 건에 대하여.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 let N = Int(readLine()!)!
-var sum = 0, mx = 0, removed = false, ans = "Kkeo-eok", i = 0
+var sum = 0, mx = 0, removed = false, ans = "Kkeo-eok"
 var C = [(x: Int, p: Int)]()
 
 for _ in 0..<N {
@@ -10,33 +10,16 @@ for _ in 0..<N {
     C.append((ip[0], ip[1]))
 }
 
-while i < N {
-    let (x, p) = C[i]
+for (x, p) in C {
     if sum <= x {
         sum += p
-        i += 1
-    } else {
-        if removed { ans = "Zzz"; break }
-        i += 1
-        removed = true
-    }
-}
-
-if ans == "Kkeo-eok" {
-    print(ans); exit(0)
-}
-
-i = 0; removed = false; ans = "Kkeo-eok"; sum = 0
-
-while i < N {
-    let (x, p) = C[i]
-    if sum <= x {
-        sum += p
-        i += 1
         mx = max(mx, p)
+    } else if sum - mx > x {
+        if removed { ans = "Zzz"; break }
+        removed = true
     } else {
         if removed { ans = "Zzz"; break }
-        sum -= mx
+        sum = min(sum - mx + p, sum)
         removed = true
     }
 }
