a4071f5544fa23c40f526dedb0e0e4ee97a4e350
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-01-01 17:42:52 +0900

    [Gold IV] Title: 알고스팟, Time: 0 ms, Memory: 2196 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.cc
+++ 백준/Gold/1261. 알고스팟/알고스팟.cc
@@ -9,8 +9,7 @@ typedef pair<int, int> pii;
 typedef tuple<int, int, int> tii;
 
 priority_queue<tii, vector<tii>, greater<tii>> pq;
-int N, M, g[101][101], md[101][101], INF=99999;
-int dy[] = {-1, 0, 1, 0}, dx[] = {0, -1, 0, 1};
+int N, M, g[101][101];
 
 int main() {
     ios::sync_with_stdio(false);
@@ -22,24 +21,20 @@ int main() {
     for (int i = 1; i <= N; i++) {
         cin >> ip;
         for (int j = 1; j <= M; j++)
-            g[i][j] = ip[j - 1] - '0',
-            md[i][j] = INF;
+            g[i][j] = ip[j - 1] - '0';
     }
 
-    md[1][1] = 0;
     pq.push({0, 1, 1});
 
     while (!pq.empty()) {
         auto [dist, y, x] = pq.top(); pq.pop();
-        if (dist > md[y][x]) continue;
+        if (y == N && x == M) {
+            cout << dist; return 0; }
         for (int i = 0; i < 4; i++) {
-            int ny = y+dy[i], nx = x+dx[i];
-            if (ny<1 || ny>N || nx<1 || nx>M) continue;
-            if (dist + g[ny][nx] >= md[ny][nx]) continue;
-            pq.push({dist + g[ny][nx], ny, nx});
-            md[ny][nx] = dist + g[ny][nx];
+            int ny = y+"2101"[i]-'1', nx = x+"1012"[i]-'1';
+            if (ny<1 || ny>N || nx<1 || nx>M || g[ny][nx]=='!') continue;
+            pq.push({dist + (g[ny][nx]==1), ny, nx});
+            g[ny][nx] = '!';
         }
     }
-
-    cout << md[N][M];
 }
