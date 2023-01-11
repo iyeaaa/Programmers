825b9cde327965e8f67727b5ec8ede5751e401a1
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-01-11 22:59:21 +0900

    [Gold V] Title: 공약수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2436. 공약수/공약수.cc
+++ 백준/Gold/2436. 공약수/공약수.cc
@@ -6,12 +6,7 @@ using namespace std;
 
 int G, L;
 
-bool isDisjoint(int a, int b) {
-    for (int k=2; k<=sqrt(min(a, b)); k++)
-        if (a%k == 0 && b%k == 0)
-            return false;
-    return true;
-}
+int gcd(int a, int b) {return b==0?a:gcd(b, a%b);}
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -19,8 +14,8 @@ int main() {
     cin >> G >> L;
     int T = L/G;
     for (int a=sqrt(T); a>=1; a--) if (T%a == 0) {
-        int b = T/a, disjoint = true;
-        if (isDisjoint(a, b)) {
+        int b = T/a;
+        if (gcd(b, a) == 1) {
             cout << a*G << ' ' << b*G;
             break;
         }
