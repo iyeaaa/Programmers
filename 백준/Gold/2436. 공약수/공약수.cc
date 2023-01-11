c28303c02797a06d64f21d85c2a6bd19445af075
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-01-11 23:04:49 +0900

    [Gold V] Title: 공약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2436. 공약수/공약수.cc
+++ 백준/Gold/2436. 공약수/공약수.cc
@@ -1,23 +1,18 @@
 
 #include <iostream>
 #include <algorithm>
-#include <cmath>
 using namespace std;
 
-int G, L;
+int G, L, T, A, B;
 
 int gcd(int a, int b) {return b==0?a:gcd(b, a%b);}
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
-    cin >> G >> L;
-    int T = L/G;
-    for (int a=sqrt(T); a>=1; a--) if (T%a == 0) {
-        int b = T/a;
-        if (gcd(b, a) == 1) {
-            cout << a*G << ' ' << b*G;
-            break;
-        }
-    }
+    cin >> G >> L; T = L/G;
+    for (int a=1,b=T/a; a*a<=T; a++,b=T/a)
+        if (T%a==0 && gcd(b, a)==1)
+            A = a*G, B = b*G;
+    cout << A << ' ' << B;
 }
