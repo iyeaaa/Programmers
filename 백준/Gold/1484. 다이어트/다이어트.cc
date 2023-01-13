a4aee8a9110a6d11d05b97fe5ee16551f4aa06c6
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-01-13 17:24:46 +0900

    [Gold V] Title: 다이어트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.cc
+++ 백준/Gold/1484. 다이어트/다이어트.cc
@@ -2,22 +2,23 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-typedef long long ll;
+
+int lo = 1, hi = 1, nothing = true;
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
-    ll G; cin >> G;
-    bool nothing = true;
-    for (ll i=1; i*i-(i-1)*(i-1)<=G; i++) {
-        ll lo=0, hi=i;
-        while (lo+1 < hi) {
-            ll mid = (lo+hi)>>1;
-            if (i*i-mid*mid >= G) lo = mid;
-            else hi = mid;
+    int G; cin >> G;
+    while (hi*hi - (hi-1)*(hi-1) <= G) {
+        if (hi*hi - lo*lo > G) {
+            lo++;
+        } else if (hi*hi - lo*lo < G) {
+            hi++;
+        } else {
+            cout << hi << '\n';
+            nothing = false;
+            hi++;
         }
-        if (lo!=0 && i*i-lo*lo == G)
-            cout << i << '\n', nothing = false;
     }
 
     if (nothing) cout << -1;
