ea471b274d6f9dc4a274e455acc2503d3f6ba8d4
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-01-28 13:31:21 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -1,33 +1,8 @@
 #include <string>
 #include <vector>
-
 using namespace std;
 
-int solution(int storey) {
-    int answer = 0;
-
-    while (storey)
-    {
-        int cur = storey % 10;
-        int next = (storey / 10) % 10;
-
-        if (cur > 5)
-        {
-            answer += 10 - cur;
-            storey += 10;
-        }
-        else if (cur == 5)
-        {
-            answer += 5;
-            storey += next >= 5 ? 10 : 0;
-        }
-        else
-        {
-            answer += cur;
-        }
-
-        storey /= 10;
-    }
-
-    return answer;
+int solution(int x) {
+    if (x/10 == 0) return min(x, 10-x+1);
+    return min(solution(x/10)+x%10, solution(x/10+1)+10-x%10);
 }
