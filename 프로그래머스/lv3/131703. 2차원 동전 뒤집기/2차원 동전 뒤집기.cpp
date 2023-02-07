fbc99c399c783146594d251b78a65d48817f48b5
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-07 20:51:17 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/lv3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.cpp
+++ 프로그래머스/lv3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.cpp
@@ -1,66 +1,60 @@
 #include <string>
 #include <vector>
+#include <bitset>
 using namespace std;
 
 const int INF = 99999999;
 int n, m;
-vector<int> flip;
 vector<vector<int>> target, beginning;
-int result = INF;
 
 // O(m)
 int make(const vector<int>& a, const vector<int>& b) {
     // 뒤집지 않아도 되는 경우
     if (a == b)
         return 0;
-    
+
     // 뒤집어서 만들 수 없는경우
     for (int i=0; i<m; i++)
         if (a[i] != 1 - b[i])
             return -1;
-    
+
     // 뒤집으면 만들 수 있는경우
     return 1;
 }
 
 // 뒤집어야 하는 횟수 반환
 // O(n*m)
-int f() {
-    int rtn = flip.size();
+int f(const bitset<100>& flip) {
+    int cnt = flip.count();
     vector<vector<int>> g = beginning;
-    
-    for (int column: flip)
-        for (int i=0; i<n; i++)
-            g[i][column] = 1 - g[i][column];
-    
+
+    for (int column=0; column<m; column++)
+        if (flip[column])
+            for (int i=0; i<n; i++)
+                g[i][column] = 1 - g[i][column];
+
     for (int i=0; i<n; i++) {
-        int cnt = make(g[i], target[i]);
-        if (cnt == -1)
+        int makeResult = make(g[i], target[i]);
+        if (makeResult == -1)
             return INF;
-        rtn += cnt;
+        cnt += makeResult;
     }
-    
-    return rtn;
+
+    return cnt;
 }
 
-// O(2^n * n * m)
-void C(int idx) {
-    result = min(result, f());
-    
-    for (int i=idx; i<m; i++) {
-        flip.push_back(i);
-        C(i+1);
-        flip.pop_back();
-    }
-}
-
-// O(2^n * n * m)
+// O(2^m * n * m)
 int solution(vector<vector<int>> tbeginning, vector<vector<int>> ttarget) {
     beginning = tbeginning;
     n = beginning.size();
     m = beginning.front().size();
     target = ttarget;
 
-    C(0);
+    int result = INF;
+    for (int i=0; i<(1<<m); i++) {
+        bitset<100> flip(i);
+        result = min(result, f(flip));
+    }
+
     return result == INF ? -1 : result;
 }
