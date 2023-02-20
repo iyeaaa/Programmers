1a31fb590a7de9bd8458432011d780271f4ff2a8
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-20 16:50:07 +0900

    [level 3] Title: 양과 늑대, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <tuple>
+using namespace std;
+typedef pair<int, int> pi;
+
+int result;
+vector<int> tree[18];
+bool v[18][(1<<17)+1];
+queue<pi> q;
+
+pi findCnt(int info, const vector<int>& isWolf) {
+    int sheep = 0;
+    int wolf = 0;
+    string bn;
+    
+    while (info > 0)
+        bn += info%2, info/=2;
+            
+    for (int i=bn.size()-1; i>=0; i--)
+        if (isWolf[i]) wolf += bn[i];
+        else sheep += bn[i];
+    
+    return {sheep, wolf};
+}
+
+int solution(vector<int> isWolf, vector<vector<int>> edges) {
+    for (const vector<int>& v: edges)
+        tree[v[0]].push_back(v[1]),
+        tree[v[1]].push_back(v[0]);
+    
+    v[0][1] = true;
+    q.push({0, 1});
+    
+    while (!q.empty()) {
+        auto [cur, info] = q.front(); q.pop();
+        auto [sheep, wolf] = findCnt(info, isWolf);
+        result = max(result, sheep);
+        
+        for (int nxt: tree[cur]) {
+            int nxtInfo = info | (1 << nxt);
+            int nxtSheep = sheep + (!isWolf[nxt] && !(info & (1<<nxt)));
+            int nxtWolf = wolf + (isWolf[nxt] && !(info & (1<<nxt)));
+            
+            
+            if (v[nxt][nxtInfo]) continue;
+            if (nxtSheep <= nxtWolf) continue;
+            
+            q.push({nxt, nxtInfo});
+            v[nxt][nxtInfo] = true;
+        }
+    }
+    
+    return result;
+}
