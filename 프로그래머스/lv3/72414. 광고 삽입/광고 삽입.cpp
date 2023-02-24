cc83a375dddceb2862f2081f16e293fb6e164549
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-24 19:54:36 +0900

    [level 3] Title: 광고 삽입, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv3/72414. 광고 삽입/광고 삽입.cpp
+++ 프로그래머스/lv3/72414. 광고 삽입/광고 삽입.cpp
@@ -1,62 +1,82 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
+typedef pair<int, int> pii;
 
-long long psum[360005];
-long long number[360005];
-long long play_time;
-long long adv_time;
-long long mxTime;
-string ans;
+int play_time, adv_time;
+vector<pii> event;
 
-string toHMS(long long second) {
-    string h = to_string(second / 3600);
-    second %= 3600;
-    while (h.size() <= 1)
-        h = '0' + h;
-    
-    string m = to_string(second / 60);
-    second %= 60;
-    while (m.size() <= 1)
-        m = '0' + m;
-    
-    string s = to_string(second);
-    while (s.size() <= 1)
-        s = '0' + s;
-    
-    return h + ':' + m + ':' + s;
+string fillup(int time1) {
+    string time = to_string(time1);
+    while (time.size() < 2)
+        time = '0' + time;
+    return time;
 }
 
-long long toSecond(const string& s, int pos) {
+string i2s(int time) {
+    string h = fillup(time / 3600);
+    time %= 3600;
+
+    string m = fillup(time / 60);
+    time %= 60;
+
+    string s = fillup(time);
+
+    return h + ':' + m + ":" + s;
+}
+
+int s2i(const string& str, int pos) {
     int rtn = 0;
-    rtn += stoi(s.substr(pos, 2))   * 3600;
-    rtn += stoi(s.substr(pos+3, 2)) * 60;
-    rtn += stoi(s.substr(pos+6, 2));
+    rtn += stoi(str.substr(pos, 2)) * 3600;
+    rtn += stoi(str.substr(pos+3, 2)) * 60;
+    rtn += stoi(str.substr(pos+6, 2));
     return rtn;
 }
 
 string solution(string p, string a, vector<string> logs) {
-    play_time = toSecond(p, 0);
-    adv_time = toSecond(a, 0);
-    
-    for (const string& s: logs) {
-        psum[toSecond(s, 0)]++;
-        psum[toSecond(s, 9)]--;
+    play_time = s2i(p, 0), adv_time = s2i(a, 0);
+
+    for (const string& log: logs) {
+        event.push_back({s2i(log, 0), 1});
+        event.push_back({s2i(log, 9), -1});
     }
-    
-    for (int i=1; i<=360000; i++)
-        psum[i] += psum[i-1];
-    
-    for (int i=1; i<=360000; i++)
-        psum[i] += psum[i-1];
-    
-    for (int i=adv_time; i<=360000; i++) {
-        long long time;
-        if ((time = psum[i-1] - psum[i-adv_time-1]) > mxTime) {
-            mxTime = time;
-            ans = toHMS(i-adv_time);
+    event.push_back({0, 0});
+
+    sort(event.begin(), event.end());
+
+    long long curtime = 0, mxtime = 0;
+    int curval = 0, mxval = 0;
+    int idx1 = 0, idx2 = 0, cnt1 = 0, cnt2 = 0;
+
+    while (idx2+1 < event.size() && event[idx2+1].first <= adv_time) {
+        curtime += cnt2 * (event[idx2+1].first - event[idx2].first);
+        cnt2 += event[idx2+1].second;
+        idx2++;
+    }
+
+    curtime += cnt2*(adv_time - event[idx2].first);
+    mxtime = curtime;
+
+    while (curval+adv_time <= play_time && idx2+1 < event.size()) {
+        int delta1 = event[idx1+1].first - curval;
+        int delta2 = event[idx2+1].first - curval - adv_time;
+        if (delta1 <= delta2) {
+            curtime += 1ll * (cnt2-cnt1) * delta1;
+            curval += delta1;
+            cnt1 += event[idx1+1].second;
+            idx1++;
+        } else {
+            curtime += 1ll * (cnt2-cnt1) * delta2;
+            curval += delta2;
+            cnt2 += event[idx2+1].second;
+            idx2++;
+        }
+        if (curtime > mxtime) {
+            mxtime = curtime;
+            mxval = curval;
         }
     }
-    
-    return ans;
+
+    return i2s(mxval);
 }
