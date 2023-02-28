49ccf3d07af717d6d9443f06dd6dfea9e93cd09a
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-03-01 02:03:59 +0900

    [unrated] Title: 대충 만든 자판, Time: 14.88 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+vector<int> ans;
+
+vector<int> solution(vector<string> keymap, vector<string> targets) {
+    for (string target: targets) {
+        int cnt = 0;
+        for (char c: target) {
+            int mnCnt = 99999999;
+            for (string k: keymap) {
+                int pos = k.find(c);
+                mnCnt = min(mnCnt, pos == string::npos ? 999999 : pos+1);
+            }
+            cnt += mnCnt;
+        }
+        cout << '\n';
+        ans.push_back(cnt > 99999 ? -1 : cnt);
+    }
+    return ans;
+}
