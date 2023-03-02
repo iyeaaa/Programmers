23987dc3303d7eb025edbc48a8ec529af411b065
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-03-03 08:55:27 +0900

    [level 2] Title: [3차] n진수 게임, Time: 0.16 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string ans;
+char out[16];
+string buf;
+
+string solution(int n, int t, int m, int p) {
+    for (int i=0; i<=9; i++)
+        out[i] = i + 48;
+    
+    for (int i=10; i<=15; i++)
+        out[i] = i + 55;
+    
+    int j = 0;
+    while (buf.size() < t*m) {
+        string two;
+        int temp = j++;
+        while (true) {
+            two += out[temp % n], temp /= n;
+            if (temp == 0) break;
+        }
+        reverse(two.begin(), two.end());
+        buf += two;
+    }
+    
+    cout << buf;
+    
+    for (int i=1; ans.size()<t; i++)
+        if (i % m == p % m) ans += buf[i-1];
+    
+    return ans;
+}
