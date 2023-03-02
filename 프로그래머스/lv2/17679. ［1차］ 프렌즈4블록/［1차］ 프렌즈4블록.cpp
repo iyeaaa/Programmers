780a9fa0b7eb84e11b6dcecf8a89f69faef8130b
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-03-02 13:48:14 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 0.17 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+
+int m, n, ans;
+vector<string> g;
+
+void fall() {
+    for (int i=m-1; i>=0; i--) for (int j=n-1; j>=0; j--) {
+        if (isalpha(g[i][j])) {
+            int ni = i;
+            while (ni+1 < m && g[ni+1][j] == '.')
+                ni++;
+            if (ni != i) g[ni][j] = g[i][j], g[i][j] = '.';
+        }
+    }
+}
+
+int rmvSquare(const vector<pii>& squares) {
+    int cnt = 0;
+    for (auto [y, x]: squares) {
+        if (isalpha(g[y][x])) g[y][x] = '.', cnt++;
+        if (isalpha(g[y+1][x])) g[y+1][x] = '.', cnt++;
+        if (isalpha(g[y][x+1])) g[y][x+1] = '.',cnt++;
+        if (isalpha(g[y+1][x+1])) g[y+1][x+1] = '.', cnt++;
+    }
+    return cnt;
+}
+
+vector<pii> findSquare() {
+    vector<pii> rtn;
+    
+    for (int i=0; i<m-1; i++) for (int j=0; j<n-1; j++) if (isalpha(g[i][j])) {
+        unordered_set<int> s;
+        s.insert(g[i][j]);
+        s.insert(g[i][j+1]);
+        s.insert(g[i+1][j]);
+        s.insert(g[i+1][j+1]);
+        
+        if (s.size() == 1)
+            rtn.push_back({i, j});
+    }
+    
+    return rtn;
+}
+
+int solution(int tm, int tn, vector<string> board) {
+    m = tm, n = tn;
+    g = board;
+    
+    while (true) {
+        vector<pii> squares = findSquare();
+        if (squares.empty()) break;
+        ans += rmvSquare(squares);
+        fall();
+    }
+    
+    return ans;
+}
