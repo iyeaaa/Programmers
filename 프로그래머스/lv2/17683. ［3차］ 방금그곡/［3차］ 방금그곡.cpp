bb4edf4988b85e23d56cde6be93f0e4c9ecd610b
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-03-02 22:36:37 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.37 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<tuple<int, int, string>> candidate;
+
+string solution(string m, vector<string> musicinfos) {
+    for (int i=m.size()-1; i>=0; i--)
+        if (m[i] == '#') {
+            m[i-1] += 'a' - 'A';
+            m.erase(i+m.begin());
+        }
+    
+    for (int i=0; i<musicinfos.size(); i++) {
+        string music = musicinfos[i];
+        int time = 0;
+        int n = 0;
+        string title, melody;
+        vector<string> vm;
+        
+        time -= stoi(music.substr(0,2)) * 60 + stoi(music.substr(3,2));
+        time += stoi(music.substr(6,2)) * 60 + stoi(music.substr(9,2));;
+        
+        int comma = music.find(',', 13);
+        title = music.substr(12, comma-12);
+        melody = music.substr(comma+1);
+        
+        for (int i=melody.size()-1; i>=0; i--)
+            if (melody[i] == '#') {
+                melody[i-1] += 'a' - 'A';
+                melody.erase(i+melody.begin());
+            }
+        
+        if (time < melody.size()) melody = melody.substr(0, time);
+        while (melody.size() < time) melody += melody;
+        
+        if (melody.find(m) != string::npos)
+            candidate.push_back({-time, i, title});
+        
+        cout << melody << '\n';
+    
+        sort(candidate.begin(), candidate.end());
+    }
+    
+    return candidate.empty() ? "(None)" : get<2>(candidate.front());
+}
