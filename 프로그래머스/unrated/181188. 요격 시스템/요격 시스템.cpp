49a9965678321a411d73b875b5309ed64c2bbc97
iyeaaa <iyeaaa@o.cnu.ac.kr>
2023-05-09 17:57:21 +0900

    [unrated] Title: 요격 시스템, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 #define F(i, l, r) for (int i = l; i < r; i++)
 using namespace std;
+typedef vector<int> vi;
+
+bool cmp(const vi& a, const vi& b) {
+    return a[1] < b[1];
+}
 
 int solution(vector<vector<int>> targets) {
-    sort(targets.begin(), targets.end());
-    priority_queue<int, vector<int>, greater<int>> pq;
-    int cnt = 0;
+    sort(targets.begin(), targets.end(), cmp);
+    int last = -1, cnt = 0;
     for (auto target: targets) {
-        if (pq.empty() || pq.top() > target[0]) pq.push(target[1]);
-        else {
-            while (!pq.empty()) pq.pop();
-            pq.push(target[1]);
-            cnt++;
-        }
+        int s = target[0], e = target[1];
+        if (last <= s) cnt++, last = e;
     }
-
-    return cnt + !pq.empty();
+    return cnt;
 }
